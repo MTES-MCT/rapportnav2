4058b489cf6b1bc72d903abea24070fea11b3da8
lhache <louis.hache.dev@protonmail.com>
2023-08-30 16:16:31 +0200

    fix local dev settings

--- backend/src/main/resources/db/migration/V0.6__add_metabase_schema.sql
+++ backend/src/main/resources/db/migration/V0.6__add_metabase_schema.sql
@@ -1,1 +1,6 @@
-CREATE SCHEMA metabase;
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'metabase') THEN
+        CREATE SCHEMA metabase;
+    END IF;
+END $$;
