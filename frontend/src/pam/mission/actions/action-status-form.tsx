915c2390b5d8d70922d86d8282327880d7c0008f
lhache <louis.hache.dev@protonmail.com>
2023-10-13 14:40:01 +0200

    Fix dae for status mutation

--- frontend/src/pam/mission/actions/action-status-form.tsx
+++ frontend/src/pam/mission/actions/action-status-form.tsx
@@ -45,9 +45,17 @@ const ActionStatusForm: React.FC<ActionStatusFormProps> = ({ action, resetSelect
   }, [action.data])
 
   const onChange = (field: string, value: any) => {
+    let date = ''
+    if (field === 'startDateTimeUtc') {
+      date = value
+    } else {
+      const savedDate = new Date(action.startDateTimeUtc!)
+      date = savedDate.toISOString()
+    }
     const updatedData = {
       missionId: missionId,
       ...omit(status, '__typename'),
+      startDateTimeUtc: date,
       [field]: value
     }
     debugger
