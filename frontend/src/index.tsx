b29ca4068a75f58be5af4351783adcb2e58cb704
lhache <louis.hache.dev@protonmail.com>
2023-10-18 10:14:35 +0200

    Add Sentry

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import reportWebVitals from './report-web-vitals'
-
 import App from './app'
+import * as Sentry from '@sentry/react'
+import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom'
+
+Sentry.init({
+  dsn: 'https://8857258f9f1549968b13e15759bdf2bc@sentry.incubateur.net/121',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // See docs for support of different versions of variation of react router
+      // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+        React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes
+      )
+    }),
+    new Sentry.Replay()
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
