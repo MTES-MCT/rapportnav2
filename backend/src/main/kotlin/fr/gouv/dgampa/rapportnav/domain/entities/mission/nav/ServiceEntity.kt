5672ef758c0e11c923125aca72a0a917b92ab968
aleckvincent <aleck.vincent@gmail.com>
2023-11-08 11:36:17 +0100

    add serviceLinked field

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/entities/mission/nav/ServiceEntity.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/entities/mission/nav/ServiceEntity.kt
@@ -5,5 +5,6 @@ import fr.gouv.dgampa.rapportnav.domain.entities.mission.nav.crew.AgentEntity
 data class ServiceEntity(
   val id: Int,
   val name: String,
-  val agents: MutableList<AgentEntity>
+  val agents: MutableList<AgentEntity>,
+  val serviceLinked: ServiceEntity?
 )
