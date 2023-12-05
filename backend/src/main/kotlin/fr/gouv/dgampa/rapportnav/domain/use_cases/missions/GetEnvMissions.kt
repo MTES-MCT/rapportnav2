135beede094214921af55b23bd76d556df9e24d1
aleckvincent <aleck.vincent@gmail.com>
2023-12-05 15:31:16 +0100

    add api call for MonitorEnv missions (GetMissions)

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/use_cases/missions/GetEnvMissions.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/use_cases/missions/GetEnvMissions.kt
@@ -1,16 +1,22 @@
 package fr.gouv.dgampa.rapportnav.domain.use_cases.missions
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import fr.gouv.dgampa.rapportnav.config.UseCase
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.MissionEntity
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.EnvMission
-import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.MissionSourceEnum
-import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.MissionTypeEnum
 import org.slf4j.LoggerFactory
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.time.ZonedDateTime
 
+
 @UseCase
-class GetEnvMissions() {
-    private val logger = LoggerFactory.getLogger(GetEnvMissions::class.java)
+class GetEnvMissions(private val mapper: ObjectMapper) {
+    private val logger = LoggerFactory.getLogger(GetEnvMissions::class.java
+    )
 
     fun execute(
         userId: Int?,
@@ -19,52 +25,17 @@ class GetEnvMissions() {
         pageNumber: Int?,
         pageSize: Int?,
     ): List<MissionEntity> {
-        // TODO add API call to MonitorEnv
 
-        val mission1 = EnvMission(
-            id = 10,
-            missionTypes = listOf(MissionTypeEnum.SEA),
-            facade = "Outre-Mer",
-            observationsCacem = null,
-            startDateTimeUtc = ZonedDateTime.parse("2022-03-15T04:50:09Z"),
-//            endDateTimeUtc = ZonedDateTime.parse("2022-03-23T20:29:03Z"),
-            isClosed = false,
-            isDeleted = false,
-            missionSource = MissionSourceEnum.RAPPORTNAV,
-            hasMissionOrder = false,
-            isUnderJdp = false,
-            isGeometryComputedFromControls = false,
-        )
-        val mission2 = EnvMission(
-            id = 11,
-            missionTypes = listOf(MissionTypeEnum.SEA),
-            facade = "Outre-Mer",
-            observationsCacem = null,
-            startDateTimeUtc = ZonedDateTime.parse("2022-02-15T04:50:09Z"),
-            endDateTimeUtc = ZonedDateTime.parse("2022-02-23T20:29:03Z"),
-            isClosed = true,
-            isDeleted = false,
-            missionSource = MissionSourceEnum.MONITORENV,
-            hasMissionOrder = false,
-            isUnderJdp = false,
-            isGeometryComputedFromControls = false
-        )
-        val mission3 = EnvMission(
-            id = 12,
-            missionTypes = listOf(MissionTypeEnum.SEA),
-            facade = "Outre-Mer",
-            observationsCacem = null,
-            startDateTimeUtc = ZonedDateTime.parse("2022-01-15T04:50:09Z"),
-            endDateTimeUtc = ZonedDateTime.parse("2022-01-23T20:29:03Z"),
-            isClosed = true,
-            isDeleted = false,
-            missionSource = MissionSourceEnum.MONITORFISH,
-            hasMissionOrder = false,
-            isUnderJdp = false,
-            isGeometryComputedFromControls = false
-        )
+        val client: HttpClient = HttpClient.newBuilder().build()
+        val request = HttpRequest.newBuilder()
+          .uri(URI.create(
+            "https://monitorenv.din.developpement-durable.gouv.fr/api/v1/missions"
+          ))
+          .build();
 
-        val envMissions = listOf(mission1, mission2, mission3)
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+        val envMissions: List<EnvMission> = mapper.readValue(response.body(), object : TypeReference<List<EnvMission>>() {})
 
         val missions = envMissions.map { MissionEntity(envMission = it) }
 
