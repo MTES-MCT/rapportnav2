7c9300c205862649104c8424a00bc6a9a0c67987
lwih <louis.hache@beta.gouv.fr>
2023-12-06 15:41:37 +0100

    Crew and Roles - rework roles and add Iris B crew

---
+++ backend/src/main/resources/db/migration/V1.2023.12.06.11.44__add_agents.sql
@@ -0,0 +1,25 @@
+DO $$
+BEGIN
+    INSERT INTO agent (first_name, last_name) VALUES
+        -- iris B:
+        ('Ivan', 'd''Alba'),
+        ('Eric', 'Bonnamy'),
+        ('Joseph', 'Le Corre'),
+        ('Bastien', 'Simonnet'),
+        ('Philippe', 'Peoc''h'),
+        ('Thierry', 'Tavernier'),
+        ('Olivier', 'Peron'),
+        ('Laurent', 'Boutet'),
+        ('Bruno', 'Achondo'),
+        ('Fabien', 'Roman'),
+        ('Dominique', 'Maingraud'),
+        ('Thierry', 'Le Berrigaud'),
+        ('Claude', 'Guyonvarch'),
+        ('Corentin', 'Peron'),
+        ('Emmanuel', 'Ferdinand'),
+        ('Jean-Philippe', 'Clauzet');
+
+
+
+
+END $$;
---
+++ backend/src/main/resources/db/migration/V1.2023.12.06.11.55__add_agent_service_link.sql
@@ -0,0 +1,22 @@
+DO $$
+BEGIN
+    -- Insert into 'agent_service' table
+INSERT INTO agent_service (service_id, agent_id) VALUES
+         -- pam_iris_B
+         (6, 76),
+         (6, 77),
+         (6, 78),
+         (6, 79),
+         (6, 80),
+         (6, 81),
+         (6, 82),
+         (6, 83),
+         (6, 84),
+         (6, 85),
+         (6, 86),
+         (6, 87),
+         (6, 88),
+         (6, 89),
+         (6, 91),
+         (6, 90);
+END $$;
