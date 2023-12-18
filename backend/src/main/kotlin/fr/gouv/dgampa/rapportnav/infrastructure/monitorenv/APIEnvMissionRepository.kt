d838448614420252f4754411380b6a26f4210794
lwih <louis.hache@beta.gouv.fr>
2023-12-18 18:24:04 +0100

    Restore mysteriously non-committed file

---
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/monitorenv/APIEnvMissionRepository.kt
@@ -0,0 +1,131 @@
+package fr.gouv.dgampa.rapportnav.infrastructure.monitorenv
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import fr.gouv.dgampa.rapportnav.domain.entities.mission.ExtendedEnvMissionEntity
+import fr.gouv.dgampa.rapportnav.domain.entities.mission.MissionEntity
+import fr.gouv.dgampa.rapportnav.domain.repositories.mission.IEnvMissionRepository
+import fr.gouv.dgampa.rapportnav.infrastructure.monitorenv.input.MissionDataOutput
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Repository
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+@Repository
+class APIEnvMissionRepository() : IEnvMissionRepository {
+    private val logger: Logger = LoggerFactory.getLogger(APIEnvMissionRepository::class.java)
+    private val zoneDateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.000X")
+
+
+    override fun findAllMissions(
+        pageNumber: Int?,
+        pageSize: Int?,
+        startedAfterDateTime: ZonedDateTime?,
+        startedBeforeDateTime: ZonedDateTime?,
+        missionSources: List<String>?,
+        missionTypes: List<String>?,
+        missionStatuses: List<String>?,
+        seaFronts: List<String>?,
+        controlUnits: List<Int>?
+    ): List<MissionEntity> {
+        val objectMapper = ObjectMapper()
+
+        // For these parameters, if the list is null or empty, we don't send the param to the server to avoid filtering results
+        val missionTypesParameter = if (!missionTypes.isNullOrEmpty()) {
+            "missionTypes=${
+                missionTypes.joinToString(
+                    ",",
+                )
+            }&"
+        } else {
+            ""
+        }
+        val missionStatusesParameter = if (!missionStatuses.isNullOrEmpty()) {
+            "missionStatus=${
+                missionStatuses.joinToString(
+                    ",",
+                )
+            }&"
+        } else {
+            ""
+        }
+        val seaFrontsParameter = if (!seaFronts.isNullOrEmpty()) "seaFronts=${seaFronts.joinToString(",")}&" else ""
+        val missionSourcesParameter = if (!missionSources.isNullOrEmpty()) {
+            "missionSource=${
+                missionSources.joinToString(
+                    ",",
+                )
+            }&"
+        } else {
+            ""
+        }
+
+        val controlUnitsParameter = if (!controlUnits.isNullOrEmpty()) {
+            "controlUnits=${
+                controlUnits.joinToString(
+                    ",",
+                )
+            }&"
+        } else {
+            ""
+        }
+
+        val missionsUrl = """
+            https://monitorenv.din.developpement-durable.gouv.fr/api/v1/missions?
+                pageNumber=${pageNumber ?: ""}&
+                pageSize=${pageSize ?: ""}&
+                startedAfterDateTime=${startedAfterDateTime?.format(zoneDateTimeFormatter) ?: ""}&
+                startedBeforeDateTime=${startedBeforeDateTime?.format(zoneDateTimeFormatter) ?: ""}&
+                $missionSourcesParameter
+                $missionTypesParameter
+                $missionStatusesParameter
+                $seaFrontsParameter
+                $controlUnitsParameter
+        """.trimIndent()
+
+        val uri = URI.create(StringBuilder(missionsUrl).toString())
+        val client: HttpClient = HttpClient.newBuilder().build()
+        val request = HttpRequest.newBuilder()
+            .uri(uri)
+            .build();
+
+        logger.info("Fetching missions at URL: $missionsUrl")
+
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+        // Check if the request was successful (status code 2xx)
+        if (response.statusCode() in 200..299) {
+            // Parse JSON response into a list of MissionDataOutput
+            val missionDataOutputList: List<MissionDataOutput> = objectMapper.readValue(response.body())
+
+            // Transform each MissionDataOutput to MissionEntity
+            val missionEntityList: List<fr.gouv.dgampa.rapportnav.domain.entities.mission.env.MissionEntity> =
+                missionDataOutputList.map { it.toMissionEntity() }
+
+            // Use the transformed data as needed
+            missionEntityList.forEach { missionEntity ->
+                logger.info("Transformed MissionEntity: $missionEntity")
+            }
+
+            val missions = missionEntityList.map {
+                MissionEntity(
+                    envMission = ExtendedEnvMissionEntity.fromEnvMission(
+                        it
+                    )
+                )
+            }
+
+            return missions
+        } else {
+            logger.info("Failed to fetch data. Status code: ${response.statusCode()}")
+            return listOf()
+        }
+
+    }
+
+}
