a2b3fb355fd534aea06306a5dc3f3534fd352ed1
lwih <louis.hache@beta.gouv.fr>
2023-12-29 14:43:41 +0100

    Try postgres:15.5-alpine

--- backend/src/test/kotlin/fr/gouv/gmampa/rapportnav/infrastructure/database/AbstractDBTests.kt
+++ backend/src/test/kotlin/fr/gouv/gmampa/rapportnav/infrastructure/database/AbstractDBTests.kt
@@ -19,7 +19,7 @@ abstract class AbstractDBTests {
 
     companion object {
         @JvmStatic
-        val container = PostgreSQLContainer("postgres:15.5-bookworm")
+        val container = PostgreSQLContainer("postgres:15.5-alpine")
             .apply {
                 withExposedPorts(5432)
                 withEnv("POSTGRES_DB", "testdb")
