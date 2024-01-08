392cd5e6345da9dcd498d1f92fb6c0648ef65bee
lwih <louis.hache@beta.gouv.fr>
2024-01-08 16:09:53 +0100

    Frontend - disable sourcemaps in prod

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,6 @@ import dotenv from 'dotenv';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: true,
     rollupOptions: {
       output: {
         format: 'es',
