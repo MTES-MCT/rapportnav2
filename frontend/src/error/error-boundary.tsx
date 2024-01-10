18a4c95c37c0336db70468aec1c23bfe10957d19
lwih <louis.hache@beta.gouv.fr>
2024-01-10 19:02:00 +0100

    Frontend - fix liint

--- frontend/src/error/error-boundary.tsx
+++ frontend/src/error/error-boundary.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-
 import { Component } from 'react'
 
 import { FrontendError } from './frontend-error.ts'
@@ -7,81 +5,79 @@ import { FrontendError } from './frontend-error.ts'
 import type { ErrorInfo, ReactNode } from 'react'
 
 export type FrontendErrorBoundaryProps = {
-  children: ReactNode
+    children: ReactNode
 }
 export type FrontendErrorBoundaryState = {
-  hasError: boolean
-  isHandled: boolean
-  message: string | undefined
+    hasError: boolean
+    isHandled: boolean
+    message: string | undefined
 }
 
 export class FrontendErrorBoundary extends Component<FrontendErrorBoundaryProps, FrontendErrorBoundaryState> {
-  constructor(props: FrontendErrorBoundaryProps) {
-    super(props)
+    constructor(props: FrontendErrorBoundaryProps) {
+        super(props)
 
-    this.state = {
-      hasError: false,
-      // eslint-disable-next-line react/no-unused-state
-      isHandled: false,
-      // eslint-disable-next-line react/no-unused-state
-      message: undefined
-    }
-  }
-
-  static getDerivedStateFromError(error: any) {
-    if (error instanceof FrontendError) {
-      return {
-        hasError: true,
-        isHandled: true,
-        message: error.message
-      }
+        this.state = {
+            hasError: false,
+            isHandled: false,
+            message: undefined
+        }
     }
 
-    return {
-      hasError: true,
-      isHandled: false,
-      message: String(error)
-    }
-  }
+    static getDerivedStateFromError(error: any) {
+        if (error instanceof FrontendError) {
+            return {
+                hasError: true,
+                isHandled: true,
+                message: error.message
+            }
+        }
 
-  override componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    if (error instanceof FrontendError) {
-      console.group('FrontendErrorBoundary > Handled Error')
-      console.debug('error', error)
-      console.debug('originalError', error.originalError)
-      console.groupEnd()
-    } else {
-      console.group('FrontendErrorBoundary > Unhandled Error')
-      console.debug('error', error)
-      console.debug('errorInfo', errorInfo)
-      console.groupEnd()
-    }
-  }
-
-  override render() {
-    const {children} = this.props
-    const {hasError, message} = this.state
-
-    if (hasError) {
-      return (
-        <div
-          style={{
-            alignItems: 'center',
-            display: 'flex',
-            flexGrow: 1,
-            height: '100%',
-            justifyContent: 'center',
-            width: '100%'
-          }}
-        >
-          Une erreur est survenue.
-          Essayez de recharger la page.
-          Si l'erreur persiste, veuillez contacter l'équipe RapportNav avec une capture d'écran.
-          {message ? `Unhandled Exception: ${message}.` : ''}
-        </div>
-      )
+        return {
+            hasError: true,
+            isHandled: false,
+            message: String(error)
+        }
     }
 
-    return children
-  }
+    override componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        if (error instanceof FrontendError) {
+            console.group('FrontendErrorBoundary > Handled Error')
+            console.debug('error', error)
+            console.debug('originalError', error.originalError)
+            console.groupEnd()
+        } else {
+            console.group('FrontendErrorBoundary > Unhandled Error')
+            console.debug('error', error)
+            console.debug('errorInfo', errorInfo)
+            console.groupEnd()
+        }
+    }
+
+    override render() {
+        const {children} = this.props
+        const {hasError, message} = this.state
+
+        if (hasError) {
+            return (
+                <div
+                    style={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexGrow: 1,
+                        height: '100%',
+                        justifyContent: 'center',
+                        width: '100%'
+                    }}
+                >
+                    Une erreur est survenue.
+                    Essayez de recharger la page.
+                    Si l'erreur persiste, veuillez contacter l'équipe RapportNav avec une capture d'écran.
+                    {message ? `Unhandled Exception: ${message}.` : ''}
+                </div>
+            )
+        }
+
+        return children
+    }
 }
