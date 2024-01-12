cc307b333bdaae1fa426dfbeef5e5558fe876d48
lwih <louis.hache@beta.gouv.fr>
2024-01-12 12:05:21 +0100

    CI - try adding coverage reports to Sonarcloud

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -2,19 +2,19 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    globals: true,
-    environment: 'jsdom',
-    server: {
-      deps: {
-        inline: ['@mtes-mct/monitor-ui']
-      }
-    },
-    coverage: {
-      provider: "istanbul",
-      reporter: ['text', 'json', 'html'],
-    },
-  }
+    plugins: [react()],
+    test: {
+        include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        globals: true,
+        environment: 'jsdom',
+        server: {
+            deps: {
+                inline: ['@mtes-mct/monitor-ui']
+            }
+        },
+        coverage: {
+            provider: "istanbul",
+            reporter: ['text', 'lcov'],
+        },
+    }
 })
