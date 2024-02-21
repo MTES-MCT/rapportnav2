6779159c44fece2dd0a7541770bf119fd73dbbb6
lwih <louis.hache@beta.gouv.fr>
2024-02-21 16:15:59 +0100

    Frontend - fix/improve refetchQueries mechanism

--- frontend/src/pam/mission/crew/use-delete-mission-crew.tsx
+++ frontend/src/pam/mission/crew/use-delete-mission-crew.tsx
@@ -1,23 +1,27 @@
 import { gql, MutationTuple, useMutation } from '@apollo/client'
 import { MissionCrew } from '../../../types/crew-types'
 import { GET_MISSION_CREW } from "./use-mission-crew.tsx";
+import { useParams } from "react-router-dom";
 
 export const MUTATION_DELETE_MISSION_CREW = gql`
-  mutation DeleteMissionCrew($id: ID!) {
-    deleteMissionCrew(id: $id)
-  }
+    mutation DeleteMissionCrew($id: ID!) {
+        deleteMissionCrew(id: $id)
+    }
 `
 
 export type DeleteMissionCrewInput = {
-  id?: string
+    id?: string
 }
 
 const useDeleteMissionCrew = (id?: string): MutationTuple<MissionCrew, Record<string, any>> => {
-  const mutation = useMutation(MUTATION_DELETE_MISSION_CREW, {
-    refetchQueries: [GET_MISSION_CREW]
-  })
+    const {missionId} = useParams()
+    const mutation = useMutation(MUTATION_DELETE_MISSION_CREW, {
+        refetchQueries: [
+            {query: GET_MISSION_CREW, variables: {missionId}},
+        ]
+    })
 
-  return mutation
+    return mutation
 }
 
 export default useDeleteMissionCrew
--- frontend/src/pam/mission/notes/use-add-update-note.tsx
+++ frontend/src/pam/mission/notes/use-add-update-note.tsx
@@ -2,6 +2,7 @@ import { gql, MutationTuple, useMutation } from '@apollo/client'
 import { GET_MISSION_TIMELINE } from "../timeline/use-mission-timeline.tsx";
 import { GET_ACTION_BY_ID } from "../actions/use-action-by-id.tsx";
 import { ActionStatus } from "../../../types/action-types.ts";
+import { useParams } from "react-router-dom";
 
 export const MUTATION_ADD_OR_UPDATE_ACTION_NOTE = gql`
     mutation AddOrUpdateNote($freeNoteAction: ActionFreeNoteInput!) {
@@ -15,8 +16,12 @@ export const MUTATION_ADD_OR_UPDATE_ACTION_NOTE = gql`
 
 
 const useAddOrUpdateNote = (): MutationTuple<ActionStatus, Record<string, any>> => {
+    const {missionId, actionId} = useParams()
     const mutation = useMutation(MUTATION_ADD_OR_UPDATE_ACTION_NOTE, {
-        refetchQueries: [GET_MISSION_TIMELINE, GET_ACTION_BY_ID]
+        refetchQueries: [
+            {query: GET_MISSION_TIMELINE, variables: {missionId}},
+            {query: GET_ACTION_BY_ID, variables: {id: actionId, missionId}},
+        ]
     })
 
     return mutation
