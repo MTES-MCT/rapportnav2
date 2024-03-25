93752d2c47571d1b01c3451afea488bd3f5ddfa1
lwih <louis.hache@beta.gouv.fr>
2024-03-25 10:51:01 +0100

    Frontend - don't fail when env theme is undefined

--- frontend/src/pam/mission/actions/action-control-env.tsx
+++ frontend/src/pam/mission/actions/action-control-env.tsx
@@ -59,13 +59,13 @@ const ActionControlEnv: React.FC<ActionControlPropsEnv> = ({action}) => {
         <Stack.Item style={{width: '100%'}}>
           <Label>Thématique de contrôle</Label>
           <Text as="h3" weight="medium" color={THEME.color.gunMetal}>
-            {actionData?.themes[0].theme ?? 'inconnue'}
+            {actionData?.themes[0]?.theme ?? 'inconnue'}
           </Text>
         </Stack.Item>
         <Stack.Item style={{width: '100%'}}>
           <Label>Sous-thématiques de contrôle</Label>
           <Text as="h3" weight="medium" color={THEME.color.gunMetal}>
-            {actionData?.themes[0].subThemes?.length
+            {actionData?.themes[0]?.subThemes?.length
               ? actionData?.themes[0].subThemes?.join(', ')
               : 'inconnues'}
           </Text>
