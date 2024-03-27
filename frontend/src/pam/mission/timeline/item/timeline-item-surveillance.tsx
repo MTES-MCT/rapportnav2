259aaee6e42ef39c1d0566af04a7d96d1b460d9e
aleckvincent <aleck.vincent@gmail.com>
2024-03-27 16:07:03 +0100

    [frontend] add MonitorEnv surveillance actions on timeline

--- frontend/src/pam/mission/timeline/item/timeline-item-surveillance.tsx
+++ frontend/src/pam/mission/timeline/item/timeline-item-surveillance.tsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import { Action } from '../../../../types/action-types.ts'
+import { useParams } from 'react-router-dom'
+import { EnvActionControl } from '../../../../types/env-mission-types.ts'
+import { TimelineItemWrapper } from './timeline-item.tsx'
+import { FlexboxGrid, Stack } from 'rsuite'
+import { Icon, THEME } from '@mtes-mct/monitor-ui'
+import Text from '../../../../ui/text.tsx'
 
-const ActionSurveillance: React.FC = () => {
-    // Implementation for ActionSurveillance
-    return <div>Hello Env Surveillance</div>
+const ActionSurveillance: React.FC<{ action: Action; onClick: any }> = ({action, onClick}) => {
+  const {actionId} = useParams()
+  const actionData = action.data as unknown as EnvActionControl
+  return (
+    <TimelineItemWrapper onClick={onClick} borderWhenSelected={action.id === actionId}>
+      <FlexboxGrid.Item
+        style={{
+          width: '100%',
+          padding: '1rem'
+        }}
+      >
+        <Stack direction="row" spacing="0.5rem">
+          <Stack.Item alignSelf="flex-start">
+            <Icon.Observation color={THEME.color.charcoal} size={20}/>
+          </Stack.Item>
+          <Stack.Item alignSelf="flex-start" style={{width: '100%'}}>
+            <Stack direction="column" spacing="0.5rem" alignItems="flex-start" style={{width: '100%'}}>
+              <Stack.Item>
+                <Stack direction="row" spacing="0.25rem">
+                  <Stack.Item>
+                    <Text as="h3" weight="medium" color={THEME.color.gunMetal}>
+                      Surveillance
+                    </Text>
+                  </Stack.Item>
+                  <Stack.Item>
+                    <Text as="h3" weight="bold" color={THEME.color.gunMetal} data-testid={"theme"}>
+                      {actionData && 'themes' in actionData && actionData?.themes[0]?.theme ? actionData?.themes[0]?.theme : ''}
+                    </Text>
+                  </Stack.Item>
+                </Stack>
+              </Stack.Item>
+
+            </Stack>
+          </Stack.Item>
+        </Stack>
+      </FlexboxGrid.Item>
+    </TimelineItemWrapper>
+  )
 }
 
 export default ActionSurveillance
