f0a46dea2431219192d72aa3e90141c9488f69f4
aleckvincent <aleck.vincent@gmail.com>
2024-04-16 17:44:29 +0200

    [backend] add missing interface IDBActionRescueRepository

---
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/database/repositories/interfaces/mission/action/IDBActionRescueRepository.kt
@@ -0,0 +1,19 @@
+package fr.gouv.dgampa.rapportnav.infrastructure.database.repositories.interfaces.mission.action
+
+import ActionRescueEntity
+import ActionRescueModel
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface IDBActionRescueRepository: JpaRepository<ActionRescueModel, UUID>{
+
+    fun findAllByMissionId(missionId: Int): List<ActionRescueModel>
+
+    override fun findById(id: UUID): Optional<ActionRescueModel>
+
+    fun save(rescueModel: ActionRescueEntity): ActionRescueModel
+
+    override fun deleteById(id: UUID)
+
+    override fun existsById(id: UUID): Boolean
+}
