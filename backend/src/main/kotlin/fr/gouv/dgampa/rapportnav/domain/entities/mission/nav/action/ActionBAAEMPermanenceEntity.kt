ab0f66b903ea3fddc5e77d6c81e89a6ededa6658
aleckvincent <aleck.vincent@gmail.com>
2024-04-24 16:39:21 +0200

    [backend] renane Action BAAEM (instead of BAEEM)

---
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/entities/mission/nav/action/ActionBAAEMPermanenceEntity.kt
@@ -0,0 +1,23 @@
+package fr.gouv.dgampa.rapportnav.domain.entities.mission.nav.action
+
+import fr.gouv.dgampa.rapportnav.infrastructure.bff.model.action.NavActionBAAEMPermanence
+import java.time.ZonedDateTime
+import java.util.*
+
+class ActionBAAEMPermanenceEntity(
+    val id: UUID,
+    val missionId: Int,
+    val startDateTimeUtc: ZonedDateTime,
+    val endDateTimeUtc: ZonedDateTime,
+    val observations: String? = null,
+) {
+    fun toNavActionBAAEMPermanence(): NavActionBAAEMPermanence {
+        return NavActionBAAEMPermanence(
+            id = id,
+            missionId = missionId,
+            startDateTimeUtc = startDateTimeUtc,
+            endDateTimeUtc = endDateTimeUtc,
+            observations = observations
+        )
+    }
+}
