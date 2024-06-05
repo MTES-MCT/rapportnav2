f99a0d565003359a47a26ed42fb873da93d47b69
lwih <louis.hache@beta.gouv.fr>
2024-06-05 13:04:13 +0200

    Release 1.5.0

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.lang.System.getenv
 
 group = "fr.gouv.dgampa"
-version = "1.4.0"
+version = "1.5.0"
 description = "RapportNav"
 
 val kotlinVersion by extra("1.9.24")
