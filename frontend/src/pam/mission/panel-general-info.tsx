10e6a02a083641f61e213ff90df86d81d6126ab3
lwih <louis.hache@beta.gouv.fr>
2024-07-10 16:23:02 +0200

    feat(Mission): enable observationsByUnit

--- frontend/src/pam/mission/panel-general-info.tsx
+++ frontend/src/pam/mission/panel-general-info.tsx
@@ -56,7 +56,7 @@ const MissionGeneralInfoPanel: React.FC<MissionGeneralInfoPanelProps> = ({ missi
             <Stack.Item style={{ width: '100%' }}>
               <MissionDistanceAndConsumption info={mission.generalInfo} />
             </Stack.Item>
-            <Stack.Item style={{ width: '100%', display: 'none' }}>
+            <Stack.Item style={{ width: '100%' }}>
               <MissionObservationsUnit missionId={mission.id} observationsByUnit={mission.observationsByUnit} />
             </Stack.Item>
           </Stack>
