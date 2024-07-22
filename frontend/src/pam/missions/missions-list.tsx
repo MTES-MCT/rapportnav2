bfbaf9e62d50cf10d922b61f143f495dcbe82290
aleckvincent <aleck.vincent@gmail.com>
2024-07-22 15:52:40 +0200

    [frontend] add new component MissionItem with export button (rapport and AEM)

--- frontend/src/pam/missions/missions-list.tsx
+++ frontend/src/pam/missions/missions-list.tsx
@@ -1,24 +1,8 @@
 import { FlexboxGrid, Stack } from 'rsuite'
-import { Link } from 'react-router-dom'
-import MissionOpenByTag from '../mission/mission-open-by-tag.tsx'
-import { Icon, THEME } from '@mtes-mct/monitor-ui'
-import { formatDateForFrenchHumans } from '../../utils/dates.ts'
+import { THEME } from '@mtes-mct/monitor-ui'
 import React from 'react'
-import styled from 'styled-components'
-import { formatMissionName } from '../mission/utils.ts'
 import { Mission } from '../../types/mission-types.ts'
-import MissionStatusTag from '../mission/mission-status-tag.tsx'
-import MissionCompletenessForStatsTag from '../mission/mission-completeness-for-stats-tag.tsx'
-
-const ListItemWithHover = styled.div`
-  height: inherit;
-  background-color: ${THEME.color.cultured};
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: ${THEME.color.blueGray25};
-  }
-`
+import MissionItem from '../mission/mission-item.tsx'
 
 interface MissionsListProps {
   missions: Mission[]
@@ -36,7 +20,7 @@ const MissionsList: React.FC<MissionsListProps> = ({ missions, prefetchMission }
           <FlexboxGrid.Item colspan={3}>
             <p style={{ color: THEME.color.slateGray, fontSize: '12px' }}>Date de début</p>
           </FlexboxGrid.Item>
-          <FlexboxGrid.Item colspan={3}>
+          <FlexboxGrid.Item colspan={4}>
             <p style={{ color: THEME.color.slateGray, fontSize: '12px' }}>Statut mission</p>
           </FlexboxGrid.Item>
           <FlexboxGrid.Item colspan={3}>
@@ -47,51 +31,8 @@ const MissionsList: React.FC<MissionsListProps> = ({ missions, prefetchMission }
         </FlexboxGrid>
       </Stack.Item>
       {missions.map((mission: Mission) => (
-        <Stack.Item key={mission.id} style={{ backgroundColor: THEME.color.cultured, width: '100%', height: '64px' }}>
-          <ListItemWithHover>
-            <Link
-              to={`/pam/missions/${mission.id}`}
-              style={{
-                textDecoration: 'none'
-              }}
-              onMouseOver={() => {
-                prefetchMission(mission.id)
-              }}
-            >
-              <FlexboxGrid align="middle" style={{ height: '100%', padding: '0.5rem 1rem' }}>
-                <FlexboxGrid.Item colspan={1} style={{ paddingTop: '8px' }}>
-                  <Icon.MissionAction size={28} color={THEME.color.charcoal} />
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={5}>
-                  <p style={{ color: THEME.color.charcoal, fontSize: '16px', fontWeight: 'bold' }}>
-                    {formatMissionName(mission.startDateTimeUtc)}
-                  </p>
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={5}>
-                  <MissionOpenByTag missionSource={mission.missionSource} isFake={mission.openBy === 'fake'} />
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={3}>
-                  <p style={{ color: THEME.color.charcoal, fontSize: '13px' }}>
-                    {' '}
-                    {formatDateForFrenchHumans(mission.startDateTimeUtc)}
-                  </p>
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={3}>
-                  <MissionStatusTag status={mission.status} />
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={3}>
-                  <MissionCompletenessForStatsTag
-                    missionStatus={mission.status}
-                    completenessForStats={mission.completenessForStats?.status}
-                  />
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={3}></FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={1}>
-                  <Icon.Edit size={20} style={{ color: THEME.color.charcoal }} />
-                </FlexboxGrid.Item>
-              </FlexboxGrid>
-            </Link>
-          </ListItemWithHover>
+        <Stack.Item style={{ width: '100%', height: '100%' }}>
+          <MissionItem mission={mission} prefetchMission={prefetchMission}></MissionItem>
         </Stack.Item>
       ))}
     </Stack>
