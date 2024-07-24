ca427b18ea15fa59661cd56459370c38fe9d81ff
Christian <christian.tonye@gmail.com>
2024-07-24 15:34:31 +0200

    feat(AEM): nbr of recognizedVessel export
     - export from mission with nbr of recognized vessel from general info

--- backend/src/test/kotlin/fr/gouv/gmampa/rapportnav/domain/entities/aem/AEMTableExportTest.kt
+++ backend/src/test/kotlin/fr/gouv/gmampa/rapportnav/domain/entities/aem/AEMTableExportTest.kt
@@ -10,6 +10,7 @@ import fr.gouv.dgampa.rapportnav.domain.entities.mission.fish.fishActions.Infrac
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.fish.fishActions.LogbookInfraction
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.fish.fishActions.OtherInfraction
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.nav.action.*
+import fr.gouv.dgampa.rapportnav.domain.entities.mission.nav.generalInfo.MissionGeneralInfoEntity
 import fr.gouv.gmampa.rapportnav.mocks.mission.EnvMissionMock
 import fr.gouv.gmampa.rapportnav.mocks.mission.NavMissionMock
 import fr.gouv.gmampa.rapportnav.mocks.mission.action.ExtendedEnvActionEntityMock
@@ -27,7 +28,46 @@ class AEMTableExportTest {
 
     @Test
     fun `Should init Table export with different values`() {
+        val mission = missionEntity()
+        val export = AEMTableExport.fromMissionAction(mission.actions!!);
+        assertThat(export).isNotNull();
+        assertThat(export.seaSafety).isNotNull();
+        assertThat(export.envTraffic).isNotNull();
+        assertThat(export.illegalFish).isNotNull();
+        assertThat(export.vesselRescue).isNotNull();
+        assertThat(export.migrationRescue).isNotNull();
+        assertThat(export.culturalMaritime).isNotNull();
+        assertThat(export.sovereignProtect).isNotNull();
+        assertThat(export.illegalImmigration).isNotNull();
+        assertThat(export.outOfMigrationRescue).isNotNull();
+        assertThat(export.pollutionControlSurveillance).isNotNull();
+        assertThat(export.notPollutionControlSurveillance).isNotNull();
+    }
 
+
+    @Test
+    fun `Should init Table export with different values from mission entity with nbrOfVesselRecognized`() {
+
+        val nbrOfRecognizedVessel = 9;
+        val mission = missionEntity();
+        mission.generalInfo?.nbrOfRecognizedVessel = nbrOfRecognizedVessel;
+        val export = AEMTableExport.fromMission(mission);
+        assertThat(export).isNotNull();
+        assertThat(export.seaSafety).isNotNull();
+        assertThat(export.envTraffic).isNotNull();
+        assertThat(export.illegalFish).isNotNull();
+        assertThat(export.vesselRescue).isNotNull();
+        assertThat(export.migrationRescue).isNotNull();
+        assertThat(export.sovereignProtect).isNotNull();
+        assertThat(export.culturalMaritime).isNotNull();
+        assertThat(export.illegalImmigration).isNotNull();
+        assertThat(export.outOfMigrationRescue).isNotNull();
+        assertThat(export.pollutionControlSurveillance).isNotNull();
+        assertThat(export.notPollutionControlSurveillance).isNotNull();
+        assertThat(export.sovereignProtect?.nbrOfRecognizedVessel).isEqualTo(nbrOfRecognizedVessel);
+    }
+
+    private fun missionEntity(): MissionEntity {
         val envMission = EnvMissionMock.create(envActions = listOf());
         val envActions = listOf(
             ExtendedEnvActionEntityMock.create(
@@ -57,7 +97,10 @@ class AEMTableExportTest {
                 )
             )
         )
-        val navMission = NavMissionMock.create(actions = navActions);
+        val navMission = NavMissionMock.create(
+            actions = navActions,
+            generalInfo = MissionGeneralInfoEntity(id = 1, missionId = 761)
+        );
         val fishMissionActions = listOf(
             ExtendedFishActionEntityMock.create(
                 controlAction = ExtendedFishActionControlEntity(
@@ -77,17 +120,6 @@ class AEMTableExportTest {
             navMission = navMission,
             fishMissionActions = fishMissionActions
         );
-        val export = AEMTableExport.fromMissionAction(mission.actions!!);
-        assertThat(export).isNotNull();
-        assertThat(export.seaSafety).isNotNull();
-        assertThat(export.envTraffic).isNotNull();
-        assertThat(export.illegalFish).isNotNull();
-        assertThat(export.vesselRescue).isNotNull();
-        assertThat(export.migrationRescue).isNotNull();
-        assertThat(export.culturalMaritime).isNotNull();
-        assertThat(export.illegalImmigration).isNotNull();
-        assertThat(export.outOfMigrationRescue).isNotNull();
-        assertThat(export.pollutionControlSurveillance).isNotNull();
-        assertThat(export.notPollutionControlSurveillance).isNotNull();
+        return mission
     }
 }
