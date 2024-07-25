14172fe8ee7895b6d2e47e921cddf71fbd84d11d
aleckvincent <aleck.vincent@gmail.com>
2024-07-25 15:44:49 +0200

    [frontend] update tests for MissionItem with Mocks

--- frontend/src/pam/mission/mission-item.test.tsx
+++ frontend/src/pam/mission/mission-item.test.tsx
@@ -1,12 +1,11 @@
 import { describe, expect, test, vi } from 'vitest'
-import { render, screen } from '../../test-utils'
+import { mockQueryResult, render, screen } from '../../test-utils'
 
 import {Mission} from '../../types/mission-types.ts'
 import { CompletenessForStatsStatusEnum, MissionStatusEnum } from '../../types/mission-types.ts'
 import MissionItem from './mission-item.tsx'
 import { fireEvent } from '@testing-library/react'
 import useIsMissionCompleteForStats from './use-is-mission-complete-for-stats.tsx'
-
 
 const mission = {
   id: 3,
@@ -19,13 +18,56 @@ const mission = {
 } as unknown as Mission
 
 
-vi.mock('./use-is-mission-complete-for-stats');
-const mockedUseIsMissionCompleteForStats = useIsMissionCompleteForStats as jest.Mock;
+
+vi.mock('./use-is-mission-complete-for-stats', async importOriginal => {
+  const actual = await importOriginal()
+  return {
+    ...actual,
+    default: vi.fn()
+  }
+})
+
+const exportLazyAEMMock = vi.fn()
+const exportLazyReportMock = vi.fn()
+vi.mock('./export/use-lazy-mission-aem-export.tsx', () => ({
+  default: () => [exportLazyAEMMock, { }]
+}))
+
+vi.mock('./export/use-lazy-mission-export.tsx', () => ({
+  default: () => [exportLazyReportMock, { }]
+}))
 
 
-describe('Export mission odt button', () => {
-  test('should render the Exporter le rapport de mission button on mouse over', () => {
-    mockedUseIsMissionCompleteForStats.mockReturnValue(true);
+describe('Mission Item component', () => {
+  afterEach(() => {
+    vi.clearAllMocks()
+    vi.resetAllMocks()
+
+    exportLazyAEMMock.mockResolvedValue({
+      data: {
+        missionAEMExport: {
+          fileName: "test.ods",
+          fileContent: "base64EncodedContent"
+        }
+      },
+      loading: false,
+      error: null
+    })
+
+    exportLazyReportMock.mockResolvedValue({
+      data: {
+        missionAEMExport: {
+          fileName: "test.odt",
+          fileContent: "base64EncodedContent"
+        }
+      },
+      loading: false,
+      error: null
+    })
+  })
+
+  test('should render the Exporter le rapport de mission button on mouse over if mission is complete for stats', () => {
+    ;(useIsMissionCompleteForStats as any).mockReturnValue(mockQueryResult(true))
 
     const { container } = render(<MissionItem mission={mission} prefetchMission={vi.fn()} />)
     const missionItemElement = container.firstChild
@@ -34,15 +76,41 @@ describe('Export mission odt button', () => {
     fireEvent.mouseOver(missionItemElement)
     expect(screen.getByText('Exporter le rapport de la mission', { exact: false })).toBeInTheDocument()
   })
-})
 
-describe('Export mission odt button not available', () => {
-  test('should not render the Exporter le rapport de mission button on mouse over', () => {
-    mockedUseIsMissionCompleteForStats.mockReturnValue(false);
-
+  test('should not render the Exporter le rapport de mission button on mouse over if the mission is not complete for stats', () => {
     const { container } = render(<MissionItem mission={mission} prefetchMission={vi.fn()} />)
     const missionItemElement = container.firstChild
     fireEvent.mouseOver(missionItemElement)
-    expect(screen.queryByText('Exporter le rapport de la mission', { exact: false })).not.toBeInTheDocument()
+    expect(screen.queryByText('Exporter le rapport de la mission', { exact: true })).not.toBeInTheDocument()
+  })
+
+  test('should trigger getMissionReport on click button Exporter le rapport de mission', () => {
+    ;(useIsMissionCompleteForStats as any).mockReturnValue(mockQueryResult(true))
+
+    const { container } = render(<MissionItem mission={mission} prefetchMission={vi.fn()} />)
+    const missionItemElement = container.firstChild
+
+    fireEvent.mouseOver(missionItemElement)
+
+    const downloadButton = screen.getByTestId('download-report-button');
+    fireEvent.click(downloadButton);
+
+    expect(exportLazyReportMock).toHaveBeenCalled()
+  })
+
+  test('should trigger getMissionAEMReport on click Télécharger les tableaux', () => {
+    ;(useIsMissionCompleteForStats as any).mockReturnValue(mockQueryResult(true))
+
+    const { container } = render(<MissionItem mission={mission} prefetchMission={vi.fn()} />)
+    const missionItemElement = container.firstChild
+
+    fireEvent.mouseOver(missionItemElement)
+
+    const downloadButton = screen.getByTestId('download-aem-button');
+    fireEvent.click(downloadButton);
+
+    expect(exportLazyAEMMock).toHaveBeenCalled()
   })
 })
+
+
