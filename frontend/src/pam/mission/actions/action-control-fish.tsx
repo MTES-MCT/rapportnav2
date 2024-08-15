59cc094bb837e9ef8c2d4bc3c29af3e1391dcf12
lwih <louis.hache@beta.gouv.fr>
2024-08-15 16:05:44 +0200

    feat(FishActions): make start/end datetimes patchable #175

--- frontend/src/pam/mission/actions/action-control-fish.tsx
+++ frontend/src/pam/mission/actions/action-control-fish.tsx
@@ -22,8 +22,8 @@ import { vesselNameOrUnknown } from './utils.ts'
 import { ActionDetailsProps } from './action-mapping.ts'
 import ActionHeader from './action-header.tsx'
 import useIsMissionFinished from '../use-is-mission-finished.tsx'
-import ActionEnvObservationsUnit from './action-env-observations-unit.tsx'
 import ActionFishObservationsUnit from './action-fish-observations-unit.tsx'
+import ActionFishDateRange from './action-fish-daterange.tsx'
 
 export const controlCheckMultiRadioOptions = Object.keys(ControlCheck).map(key => {
   let label
@@ -95,16 +95,11 @@ const ActionControlFish: React.FC<ActionControlPropsFish> = ({ action }) => {
           </Stack>
         </Stack.Item>
         <Stack.Item style={{ width: '100%' }}>
-          <Label>Date et heure</Label>
-          <DatePicker
-            defaultValue={fishAction.startDateTimeUtc}
-            // label="Date et heure"
-            withTime={true}
-            isCompact={false}
-            isLight={true}
-            name="startDateTimeUtc"
-            readOnly={true}
-            disabled={true}
+          <ActionFishDateRange
+            missionId={missionId}
+            actionId={actionId}
+            startDateTimeUtc={fishAction.startDateTimeUtc}
+            endDateTimeUtc={fishAction.endDateTimeUtc}
           />
         </Stack.Item>
         <Stack.Item>
