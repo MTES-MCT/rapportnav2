1785e9769d017c59e38c698cdd8810b92eb0b7f6
lwih <louis.hache@beta.gouv.fr>
2024-08-22 12:16:59 +0200

    Release 1.7.6

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.lang.System.getenv
 
 group = "fr.gouv.dgampa"
-version = "1.7.5"
+version = "1.7.6"
 description = "RapportNav"
 
 val kotlinVersion by extra("1.9.24")
