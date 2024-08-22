d39e56b4d312bf0fd50bdb60831acce31496c989
lwih <louis.hache@beta.gouv.fr>
2024-08-22 15:13:46 +0200

    fix(backend): set data instead of abstract class for MissionEnvActionDataOutput

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/monitorenv/output/action/MissionEnvActionDataOutput.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/monitorenv/output/action/MissionEnvActionDataOutput.kt
@@ -5,7 +5,7 @@ import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.envActions.PatchedE
 import java.time.ZonedDateTime
 import java.util.*
 
-abstract class MissionEnvActionDataOutput(
+data class MissionEnvActionDataOutput(
     open val id: UUID,
     open val actionStartDateTimeUtc: ZonedDateTime? = null,
     open val actionEndDateTimeUtc: ZonedDateTime? = null,
