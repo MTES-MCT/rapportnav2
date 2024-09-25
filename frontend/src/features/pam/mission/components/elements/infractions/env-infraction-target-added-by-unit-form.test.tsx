feeee705cd4686e2dfde70b321d257b6fecddcdd
aleckvincent <aleck.vincent@gmail.com>
2024-09-25 15:35:51 +0200

    feature(347) add check on ActionTargetType

--- frontend/src/features/pam/mission/components/elements/infractions/env-infraction-target-added-by-unit-form.test.tsx
+++ frontend/src/features/pam/mission/components/elements/infractions/env-infraction-target-added-by-unit-form.test.tsx
@@ -2,6 +2,7 @@ import { render, screen, fireEvent } from '../../../../../../test-utils.tsx'
 import EnvInfractionTargetAddedByUnitForm from './env-infraction-target-added-by-unit-form.tsx'
 import { ControlType } from '@common/types/control-types.ts'
 import {
+  ActionTargetTypeEnum,
   FormalNoticeEnum,
   InfractionTypeEnum, VehicleTypeEnum,
   VesselSizeEnum,
@@ -9,14 +10,23 @@ import {
 } from '@common/types/env-mission-types.ts'
 import { Natinf } from '@common/types/infraction-types.ts'
 
-const infractionTargetMock = {
+const targetVesselMock = {
   id: '123',
   vehicleType: VehicleTypeEnum.VESSEL
 }
 
-const infractionPersonTargetMock = {
+const targetMock = {
   id: '123',
   vehicleType: null
+}
+
+const infractionVesselMock = {
+  id: '123',
+  controlType: ControlType.ADMINISTRATIVE,
+  infractionType: InfractionTypeEnum.WITHOUT_REPORT,
+  natinfs: ['123'],
+  observations: undefined,
+  target: targetVesselMock
 }
 
 const infractionMock = {
@@ -25,19 +35,11 @@ const infractionMock = {
   infractionType: InfractionTypeEnum.WITHOUT_REPORT,
   natinfs: ['123'],
   observations: undefined,
-  target: infractionTargetMock
+  target: targetMock
 }
 
-const infractionPersonMock = {
-  id: '123',
-  controlType: ControlType.ADMINISTRATIVE,
-  infractionType: InfractionTypeEnum.WITHOUT_REPORT,
-  natinfs: ['123'],
-  observations: undefined,
-  target: infractionPersonTargetMock
-}
-
-const props = (infraction = infractionMock) => ({
+const propsVehicleVessel = (infraction = infractionVesselMock) => ({
+  actionTargetType: ActionTargetTypeEnum.VEHICLE,
   infraction,
   availableControlTypesForInfraction: [ControlType.ADMINISTRATIVE],
   onChange: vi.fn(),
@@ -45,22 +47,42 @@ const props = (infraction = infractionMock) => ({
   onCancel: vi.fn()
 })
 
-const propsPerson = (infraction = infractionPersonMock) => ({
+const propsVehicleWithoutVessel = (infraction = infractionMock) => ({
+  actionTargetType: ActionTargetTypeEnum.VEHICLE,
   infraction,
   availableControlTypesForInfraction: [ControlType.ADMINISTRATIVE],
   onChange: vi.fn(),
   onChangeTarget: vi.fn(),
   onCancel: vi.fn()
 })
+
+const propsCompany = (infraction = infractionMock) => ({
+  actionTargetType: ActionTargetTypeEnum.COMPANY,
+  infraction,
+  availableControlTypesForInfraction: [ControlType.ADMINISTRATIVE],
+  onChange: vi.fn(),
+  onChangeTarget: vi.fn(),
+  onCancel: vi.fn()
+})
+
+const propsIndividual = (infraction = infractionMock) => ({
+  actionTargetType: ActionTargetTypeEnum.INDIVIDUAL,
+  infraction,
+  availableControlTypesForInfraction: [ControlType.ADMINISTRATIVE],
+  onChange: vi.fn(),
+  onChangeTarget: vi.fn(),
+  onCancel: vi.fn()
+})
+
 describe('EnvInfractionTargetAddedByUnitForm', () => {
   test('renders the component', async () => {
-    render(<EnvInfractionTargetAddedByUnitForm {...props()} />)
+    render(<EnvInfractionTargetAddedByUnitForm {...propsVehicleVessel()} />)
     expect(screen.getByText('Type de contrôle avec infraction')).toBeInTheDocument()
   })
   describe('changing the target data', () => {
     test('calls onChangeTarget when changing vesselIdentifier', async () => {
       const onChangeTarget = vi.fn()
-      render(<EnvInfractionTargetAddedByUnitForm {...{ ...props(), onChangeTarget }} />)
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsVehicleVessel(), onChangeTarget }} />)
       const field = screen.getByRole('vesselIdentifier')
       const value = 'test'
       fireEvent.change(field, { target: { value } })
@@ -69,7 +91,7 @@ describe('EnvInfractionTargetAddedByUnitForm', () => {
     })
     test('calls onChangeTarget when changing identityControlledPerson', async () => {
       const onChangeTarget = vi.fn()
-      render(<EnvInfractionTargetAddedByUnitForm {...{ ...props(), onChangeTarget }} />)
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsVehicleVessel(), onChangeTarget }} />)
       const field = screen.getByRole('identityControlledPerson')
       const value = 'test'
       fireEvent.change(field, { target: { value } })
@@ -77,22 +99,87 @@ describe('EnvInfractionTargetAddedByUnitForm', () => {
       expect(onChangeTarget).toHaveBeenCalled()
     })
 
-    test('should not display vessel or vehicle inputs when VehicleType is null', async () => {
+    test('should display vessel inputs when VehicleType is VESSEL', async () => {
       const onChangeTarget = vi.fn()
-      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsPerson(), onChangeTarget }} />)
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsVehicleVessel(), onChangeTarget }} />)
+
+      const inputsVessel = screen.queryByTestId('stack-vessel-infraction-env')
+
+      expect(inputsVessel).toBeInTheDocument()
+    })
+
+    test('should not display vessel inputs when VehicleType is not VESSEL', async () => {
+      const onChangeTarget = vi.fn()
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsVehicleWithoutVessel(), onChangeTarget }} />)
 
       const inputsVessel = screen.queryByTestId('stack-vessel-infraction-env')
 
       expect(inputsVessel).toBeNull()
     })
 
-    test('should display vessel or vehicle inputs when VehicleType is not null', async () => {
+    test('should display vesselIdentifier input when ActionTargetType is VEHICLE', async () => {
       const onChangeTarget = vi.fn()
-      render(<EnvInfractionTargetAddedByUnitForm {...{ ...props(), onChangeTarget }} />)
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsVehicleWithoutVessel(), onChangeTarget }} />)
 
-      const inputsVessel = screen.queryByTestId('stack-vessel-infraction-env')
+      const inputsVessel = screen.queryByTestId('vessel-identifier')
 
       expect(inputsVessel).toBeInTheDocument()
     })
+
+    test('should not display vessel inputs when ActionTargetType is COMPANY', async () => {
+      const onChangeTarget = vi.fn()
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsCompany(), onChangeTarget }} />)
+
+      const inputsVessel = screen.queryByTestId('stack-vessel-infraction-env')
+
+      expect(inputsVessel).toBeNull()
+    })
+
+    test('should not display vesselIdentifier input when ActionTargetType is COMPANY', async () => {
+      const onChangeTarget = vi.fn()
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsCompany(), onChangeTarget }} />)
+
+      const inputsVessel = screen.queryByTestId('vessel-identifier')
+
+      expect(inputsVessel).toBeNull()
+    })
+
+    test('should not display vessel inputs when ActionTargetType is INDIVIDUAL', async () => {
+      const onChangeTarget = vi.fn()
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsIndividual(), onChangeTarget }} />)
+
+      const inputsVessel = screen.queryByTestId('stack-vessel-infraction-env')
+
+      expect(inputsVessel).toBeNull()
+    })
+
+    test('should not display vesselIdentifier input when ActionTargetType is INDIVIDUAL', async () => {
+      const onChangeTarget = vi.fn()
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsIndividual(), onChangeTarget }} />)
+
+      const inputsVessel = screen.queryByTestId('vessel-identifier')
+
+      expect(inputsVessel).toBeNull()
+    })
+
+    test('should display label "Identité de la personne contrôlée" input when ActionTargetType is INDIVIDUAL', async () => {
+      const onChangeTarget = vi.fn()
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsIndividual(), onChangeTarget }} />)
+
+      const label = screen.getByLabelText(/Identité de la personne contrôlée/i)
+
+      expect(label).toBeInTheDocument()
+    })
+
+    test('should display label "Identité de la personne morale contrôlée" when ActionTargetType is COMPANY', async () => {
+      const onChangeTarget = vi.fn()
+      render(<EnvInfractionTargetAddedByUnitForm {...{ ...propsCompany(), onChangeTarget }} />)
+
+      const label = screen.getByLabelText(/Identité de la personne morale contrôlée/i)
+
+      expect(label).toBeInTheDocument()
+    })
+
+
   })
 })
--- frontend/src/features/pam/mission/components/elements/infractions/env-infraction-target-added-by-unit-form.tsx
+++ frontend/src/features/pam/mission/components/elements/infractions/env-infraction-target-added-by-unit-form.tsx
@@ -11,6 +11,7 @@ import {
   VESSEL_TYPE_OPTIONS
 } from '../../../utils/control-utils.ts'
 import { VesselSizeEnum, VesselTypeEnum } from '@common/types/mission-types.ts'
+import { ActionTargetTypeEnum, VehicleTypeEnum } from '@common/types/env-mission-types.ts'
 
 interface EnvInfractionNewTargetFormProps {
   infraction?: Infraction
@@ -18,6 +19,7 @@ interface EnvInfractionNewTargetFormProps {
   onChange: (field: string, value: any) => void
   onChangeTarget: (field: string, value: any) => void
   onCancel: () => void
+  actionTargetType?: ActionTargetTypeEnum
 }
 
 const EnvInfractionTargetAddedByUnitForm: React.FC<EnvInfractionNewTargetFormProps> = ({
@@ -25,7 +27,8 @@ const EnvInfractionTargetAddedByUnitForm: React.FC<EnvInfractionNewTargetFormPro
   availableControlTypesForInfraction,
   onChange,
   onChangeTarget,
-  onCancel
+  onCancel,
+  actionTargetType
 }) => {
   const [identityControlledPersonValue, setIdentityControlledPersonValue] = useState<string | undefined>(undefined)
   const [vesselIdentifierValue, setVesselIdentifierValue] = useState<string | undefined>(undefined)
@@ -63,7 +66,8 @@ const EnvInfractionTargetAddedByUnitForm: React.FC<EnvInfractionNewTargetFormPro
       </Stack.Item>
       <Stack.Item style={{ width: '100%' }}>
         <Stack direction="column" alignItems="baseline" spacing={'0.5rem'}>
-          {infraction?.target?.vehicleType && (
+
+          {infraction?.target?.vehicleType === VehicleTypeEnum.VESSEL && (
             <Stack.Item style={{ width: '100%' }} data-testid={"stack-vessel-infraction-env"}>
               <Stack direction="row" alignItems="baseline" spacing={'0.5rem'}>
                 <Stack.Item style={{ width: '40%' }}>
@@ -86,26 +90,34 @@ const EnvInfractionTargetAddedByUnitForm: React.FC<EnvInfractionNewTargetFormPro
                     onChange={(nextValue: VesselTypeEnum | undefined) => onChangeTarget('vesselType', nextValue)}
                   />
                 </Stack.Item>
-                <Stack.Item style={{ width: '100%' }}>
-                  <TextInput
-                    label="Immatriculation"
-                    isRequired={true}
-                    value={vesselIdentifierValue}
-                    name="vesselIdentifier"
-                    role="vesselIdentifier"
-                    onChange={handleVesselIdentifierChange}
-                    onBlur={handleVesselIdentifierBlur}
-                  />
-                </Stack.Item>
               </Stack>
             </Stack.Item>
           )}
 
-          <Stack.Item style={{ width: '100%' }}>
+          {(actionTargetType === ActionTargetTypeEnum.VEHICLE) && (
+            <Stack.Item style={{ width: '100%' }}>
+              <TextInput
+                label="Immatriculation"
+                isRequired={true}
+                value={vesselIdentifierValue}
+                name="vesselIdentifier"
+                role="vesselIdentifier"
+                onChange={handleVesselIdentifierChange}
+                onBlur={handleVesselIdentifierBlur}
+                data-testid={'vessel-identifier'}
+              />
+            </Stack.Item>
+          )}
+
+          <Stack.Item style={{ width: '100%', marginTop: '10px' }}>
             <Stack direction="row" alignItems="baseline" spacing={'0.5rem'}>
               <Stack.Item style={{ width: '100%' }}>
                 <TextInput
-                  label="Identité de la personne contrôlée"
+                  label={
+                  actionTargetType === ActionTargetTypeEnum.COMPANY
+                    ? "Identité de la personne morale contrôlée"
+                    : "Identité de la personne contrôlée"
+                }
                   isRequired={true}
                   value={identityControlledPersonValue}
                   name="identityControlledPerson"
