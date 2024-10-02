f5653a039034a2f8dcc5080d5ff4ee53e57bebe5
Christian <christian.tonye@gmail.com>
2024-10-02 10:46:40 +0200

    fix(build): update import

--- frontend/src/features/pam/mission/components/elements/actions/action-surveillance-env.test.tsx
+++ frontend/src/features/pam/mission/components/elements/actions/action-surveillance-env.test.tsx
@@ -1,15 +1,10 @@
-import { render, screen } from '../../../../../../test-utils.tsx'
-import { ActionTypeEnum, EnvAction, MissionSourceEnum } from '@common/types/env-mission-types.ts'
 import { ActionStatusType } from '@common/types/action-types.ts'
-import { vi } from 'vitest'
+import { ActionTypeEnum, EnvAction, MissionSourceEnum } from '@common/types/env-mission-types.ts'
 import * as useActionByIdModule from '@features/pam/mission/hooks/use-action-by-id'
 import { GraphQLError } from 'graphql/error'
 import { vi } from 'vitest'
-import { render, screen } from '../../../test-utils'
-import { Action, ActionStatusType } from '../../../types/action-types.ts'
-import { ActionTypeEnum, EnvAction, MissionSourceEnum } from '../../../types/env-mission-types.ts'
+import { render, screen } from '../../../../../../test-utils.tsx'
 import ActionSurveillanceEnv from './action-surveillance-env.tsx'
-import useActionById from './use-action-by-id.tsx'
 
 const actionMock = {
   id: '1',
