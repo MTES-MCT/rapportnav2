6249d26bc6b9e4a0c85e8ea88061f96acf2709d9
aleckvincent <aleck.vincent@gmail.com>
2024-10-28 16:32:00 +0100

    add title "Mes rapports de mission"

--- frontend/src/v2/features/ulam/components/element/mission-list-ulam.tsx
+++ frontend/src/v2/features/ulam/components/element/mission-list-ulam.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { JSX } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { Col, FlexboxGrid, Loader, Stack } from 'rsuite'
+import { Col, Container, FlexboxGrid, Loader, Stack } from 'rsuite'
 import MissionListing from '../../../common/components/elements/mission-listing.tsx'
 import MissionListDaterangeNavigator from '../../../common/components/elements/mission-list-daterange-navigator.tsx'
-
-const MissionListUlam: React.FC = () => {
+import { Icon } from '@mtes-mct/monitor-ui'
+interface MissionListUlamProps  {
+  dateRangeNavigator: JSX.Element,
+  missionListing: JSX.Element
+}
+const MissionListUlam: React.FC<MissionListUlamProps> = ({dateRangeNavigator, missionListing}) => {
   const navigate = useNavigate()
   return (
     <>
       <FlexboxGrid
-        // align="middle"
         justify="center"
         style={{ padding: '4rem 2rem', display: 'flex', flex: 1 }}
       >
         <FlexboxGrid.Item as={Col} colspan={24} xxl={23}>
-          <MissionListDaterangeNavigator />
-          <MissionListing/>
+          <Container>
+            <h3 style={{marginBottom: '45px'}}><Icon.MissionAction size={26}/> Missions</h3>
+            <h4 style={{marginBottom: '25px'}}>Mes rapports de mission</h4>
+          </Container>
+          {dateRangeNavigator}
+          <Container>
+            {missionListing}
+          </Container>
         </FlexboxGrid.Item>
       </FlexboxGrid>
     </>
