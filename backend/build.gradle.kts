1c452a741dbee58f00d1384e5d0428f2983946ff
lwih <louis.hache@beta.gouv.fr>
2024-11-06 15:57:11 +0100

    chore(deps): bump spring-boot

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -7,7 +7,7 @@ description = "RapportNav"
 
 val kotlinVersion by extra("1.9.24")
 val serializationVersion by extra("1.6.2")
-val springVersion by extra("3.3.3")
+val springVersion by extra("3.3.5")
 val testcontainersVersion by extra("1.19.3")
 val flywayVersion by extra("10.10.0")
 
@@ -17,7 +17,7 @@ plugins {
   kotlin("jvm") version "1.9.24"
   kotlin("plugin.spring") version "1.9.24"
   kotlin("plugin.jpa") version "1.9.24"
-  id("org.springframework.boot") version "3.3.3"
+  id("org.springframework.boot") version "3.3.5"
   id("io.spring.dependency-management") version "1.1.4"
   id("org.owasp.dependencycheck") version "8.4.0"
   id("org.flywaydb.flyway") version "10.10.0"
