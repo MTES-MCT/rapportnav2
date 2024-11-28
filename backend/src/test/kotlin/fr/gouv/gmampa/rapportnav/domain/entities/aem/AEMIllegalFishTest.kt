54d212c57f7724bc3f15afe955c94cee326c722a
Christian <christian.tonye@gmail.com>
2024-11-28 11:03:58 +0100

    feat(AEM): update  calcul to improve to the number of natinf
     - Add the number of natinf not the natInf itself
     - Update test

--- backend/src/test/kotlin/fr/gouv/gmampa/rapportnav/domain/entities/aem/AEMIllegalFishTest.kt
+++ backend/src/test/kotlin/fr/gouv/gmampa/rapportnav/domain/entities/aem/AEMIllegalFishTest.kt
@@ -22,8 +22,8 @@ class AEMIllegalFishTest {
 
         assertThat(illegalImmigration).isNotNull();
         assertThat(illegalImmigration.quantityOfFish).isEqualTo(6.0);
-        assertThat(illegalImmigration.nbrOfInfraction).isEqualTo(7.0);
         assertThat(illegalImmigration.nbrOfHourAtSea).isEqualTo(3.0);
+        assertThat(illegalImmigration.nbrOfInfraction).isEqualTo(4.0);
         assertThat(illegalImmigration.nbrOfPolFishAction).isEqualTo(2.0);
         assertThat(illegalImmigration.nbrOfInfractionWithPV).isEqualTo(4.0);
     }
@@ -36,7 +36,7 @@ class AEMIllegalFishTest {
                         speciesInfractions = listOf(),
                         gearInfractions = listOf(GearInfraction(infractionType = InfractionType.WITH_RECORD)),
                         otherInfractions = listOf(OtherInfraction()),
-                        logbookInfractions = listOf(LogbookInfraction(infractionType = InfractionType.WITHOUT_RECORD)),
+                        logbookInfractions = listOf(LogbookInfraction(infractionType = InfractionType.WITHOUT_RECORD, natinf = 27688)),
                         actionDatetimeUtc = Instant.parse("2019-09-09T00:00:00.000+01:00"),
                         actionEndDatetimeUtc = Instant.parse("2019-09-09T01:00:00.000+01:00"),
                         speciesQuantitySeized = 4
@@ -47,11 +47,11 @@ class AEMIllegalFishTest {
                 controlAction = ExtendedFishActionControlEntity(
                     action = FishActionControlMock.create(
                         logbookInfractions = listOf(LogbookInfraction()),
-                        gearInfractions = listOf(GearInfraction(infractionType = InfractionType.WITH_RECORD, natinf = 4)),
-                        otherInfractions = listOf(OtherInfraction(infractionType = InfractionType.WITH_RECORD, natinf = 1)),
+                        gearInfractions = listOf(GearInfraction(infractionType = InfractionType.WITH_RECORD, natinf = 49558)),
+                        otherInfractions = listOf(OtherInfraction(infractionType = InfractionType.WITH_RECORD, natinf = 98308)),
                         speciesInfractions = listOf(
                             SpeciesInfraction(infractionType = InfractionType.WITH_RECORD),
-                            SpeciesInfraction(infractionType = InfractionType.WITHOUT_RECORD, natinf = 2)
+                            SpeciesInfraction(infractionType = InfractionType.WITHOUT_RECORD, natinf = 203839)
                         ),
                         actionDatetimeUtc = Instant.parse("2019-09-09T02:00:00.000+01:00"),
                         actionEndDatetimeUtc = Instant.parse("2019-09-09T04:00:00.000+01:00"),
