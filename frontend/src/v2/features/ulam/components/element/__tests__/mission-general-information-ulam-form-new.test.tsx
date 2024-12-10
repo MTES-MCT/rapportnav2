ff9771381d65730f3d6ef161d64b0ec3656fcffd
aleckvincent <aleck.vincent@gmail.com>
2024-12-10 11:17:37 +0100

    add tests for mission general info initial form

---
+++ frontend/src/v2/features/ulam/components/element/__tests__/mission-general-information-ulam-form-new.test.tsx
@@ -0,0 +1,41 @@
+
+import { MissionTypeEnum } from '@common/types/env-mission-types'
+import MissionGeneralInformationUlamFormNew from '../mission-general-information-ulam-form-new.tsx'
+import { render, fireEvent, screen } from '../../../../../../test-utils.tsx'
+
+
+
+describe('MissionGeneralInformationUlamFormNew', () => {
+  it('renders the form with initial values', () => {
+    render(
+      <MissionGeneralInformationUlamFormNew
+        startDateTimeUtc="2024-01-01T00:00:00Z"
+        endDateTimeUtc="2024-01-01T00:00:00Z"
+        missionType={[MissionTypeEnum.AIR]}
+        onClose={vi.fn()}
+      />
+    )
+
+    expect(screen.getByText('Type de mission')).toBeInTheDocument()
+  })
+
+  it('check if form submitted', () => {
+    const consoleSpy = vi.spyOn(console, 'log').mockImplementation(() => {});
+
+    render(
+      <MissionGeneralInformationUlamFormNew
+        startDateTimeUtc="2024-01-01T00:00:00Z"
+        endDateTimeUtc="2024-01-01T00:00:00Z"
+        missionType={[MissionTypeEnum.AIR]}
+        onClose={vi.fn()}
+      />
+    );
+
+    fireEvent.click(screen.getByText('Créer le rapport'));
+
+    // TODO: a remplacer par le check d'un appel api
+    expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('Form Submitted'), expect.any(Object));
+
+    consoleSpy.mockRestore();
+  });
+})
