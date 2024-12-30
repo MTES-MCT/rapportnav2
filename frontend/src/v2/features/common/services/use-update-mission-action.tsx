7f36dbee510a46261ef965a2537af5e13b0994aa
Christian <christian.tonye@gmail.com>
2024-12-30 17:07:40 +0100

    feat(services): front service with REST
     - create actions
     - get list of actions
     - get action by id
     - update actions

--- frontend/src/v2/features/common/services/use-update-mission-action.tsx
+++ frontend/src/v2/features/common/services/use-update-mission-action.tsx
@@ -1,38 +1,22 @@
-import { gql, MutationTuple, useMutation } from '@apollo/client'
-import { GET_ACTION } from './use-mission-action'
-import { GET_ACTION_LIST } from './use-mission-action-list'
-
-export const MUTATION_UPDATE_MISSION_ACTION = gql`
-  mutation UpdateMissionAction($input: MissionActionInput!) {
-    updateMissionAction(action: $input) {
-      id
-      missionId
-      source
-      summaryTags
-      actionType
-      status
-      controlsToComplete
-      isCompleteForStats
-      completenessForStats {
-        status
-        sources
-      }
-      sourcesOfMissingDataForStats
-    }
-  }
-`
+import { useMutation, UseMutationResult, useQueryClient } from '@tanstack/react-query'
+import axios from '../../../../query-client/axios'
+import { MissionAction } from '../types/mission-action'
 
 const useUpdateMissionActionMutation = (
-  actionId: string,
-  missionId?: number
-): MutationTuple<unknown, Record<string, any>> => {
-  const mutation = useMutation(MUTATION_UPDATE_MISSION_ACTION, {
-    refetchQueries: [
-      { query: GET_ACTION_LIST, variables: { missionId } },
-      { query: GET_ACTION, variables: { actionId, missionId } }
-    ]
-  })
+  missionId: number,
+  actionId?: string
+): UseMutationResult<MissionAction, Error, MissionAction, unknown> => {
+  const queryClient = useQueryClient()
 
+  const updateAction = (action: MissionAction): Promise<MissionAction> =>
+    axios.put(`missions/${missionId}/actions/${actionId}`, action).then(response => response.data)
+
+  const mutation = useMutation({
+    mutationFn: updateAction,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['actions'] })
+    }
+  })
   return mutation
 }
 
