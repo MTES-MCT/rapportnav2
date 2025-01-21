d79fdb3abea8b18bb2991409d66c2b524d55d6cb
lwih <louis.hache@beta.gouv.fr>
2025-01-21 12:03:38 +0100

    fix: redirect PAM to v1 homepage

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,6 +1,6 @@
 import AuthGuard from '@features/auth/components/auth-guard.tsx'
 import * as Sentry from '@sentry/react'
-import { createBrowserRouter } from 'react-router-dom'
+import {createBrowserRouter} from 'react-router-dom'
 import ErrorPage from '../pages/error-page.tsx'
 import Login from '../pages/login.tsx'
 import MissionPage from '../pages/mission-page.tsx'
@@ -15,10 +15,10 @@ import MissionUlamPage from '../v2/pages/mission-ulam-page.tsx'
 export const getPath = (path: string) => `/${path}`
 
 export const ROOT_PATH = '/'
-export const LOGIN_PATH = 'login'
-export const SIGNUP_PATH = 'signup'
+export const LOGIN_PATH = '/login'
+export const SIGNUP_PATH = '/signup'
 export const PAM_HOME_PATH = '/pam/missions'
-export const PAM_V2_HOME_PATH = 'v2/pam/missions'
+export const PAM_V2_HOME_PATH = '/v2/pam/missions'
 export const ULAM_V2_HOME_PATH = '/v2/ulam/missions'
 
 const sentryCreateBrowserRouter = Sentry.wrapCreateBrowserRouter(createBrowserRouter)
@@ -26,31 +26,31 @@ const sentryCreateBrowserRouter = Sentry.wrapCreateBrowserRouter(createBrowserRo
 export const router = sentryCreateBrowserRouter([
   {
     path: ROOT_PATH,
-    element: <Home2 />,
-    errorElement: <ErrorPage />
+    element: <Home2/>,
+    errorElement: <ErrorPage/>
   },
   {
     path: LOGIN_PATH,
-    element: <Login />
+    element: <Login/>
   },
   {
     path: SIGNUP_PATH,
-    element: <SignUp />
+    element: <SignUp/>
   },
   {
     path: PAM_HOME_PATH,
     element: (
       <AuthGuard>
-        <MissionsPage />
+        <MissionsPage/>
       </AuthGuard>
     ),
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage/>
   },
   {
     path: 'pam/missions/:missionId/:actionId?',
     element: (
       <AuthGuard>
-        <MissionPage />
+        <MissionPage/>
       </AuthGuard>
     )
   },
@@ -59,36 +59,36 @@ export const router = sentryCreateBrowserRouter([
     path: ULAM_V2_HOME_PATH,
     element: (
       <AuthGuard>
-        <MissionListUlamPage />
+        <MissionListUlamPage/>
       </AuthGuard>
     ),
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage/>
   },
   {
     path: `${ULAM_V2_HOME_PATH}/:missionId/:actionId?`,
     element: (
       <AuthGuard>
-        <MissionUlamPage />
+        <MissionUlamPage/>
       </AuthGuard>
     ),
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage/>
   },
   {
     path: PAM_V2_HOME_PATH,
     element: (
       <AuthGuard>
-        <MissionListPamPage />
+        <MissionListPamPage/>
       </AuthGuard>
     ),
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage/>
   },
   {
     path: `${PAM_V2_HOME_PATH}/:missionId/:actionId?`,
     element: (
       <AuthGuard>
-        <MissionPamPage />
+        <MissionPamPage/>
       </AuthGuard>
     ),
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage/>
   }
 ])
--- frontend/src/v2/pages/home.tsx
+++ frontend/src/v2/pages/home.tsx
@@ -1,8 +1,10 @@
-import { FC, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import {FC, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 import useAuth from '../features/auth/hooks/use-auth'
-import { ModuleType } from '../features/common/types/module-type'
-import { RoleType } from '../features/common/types/role-type'
+import {ModuleType} from '../features/common/types/module-type'
+import {RoleType} from '../features/common/types/role-type'
+import {PAM_HOME_PATH, ULAM_V2_HOME_PATH} from "@router/router.tsx";
+
 
 const ROUTES = {
   [RoleType.ADMIN]: ModuleType.PAM,
@@ -12,18 +14,24 @@ const ROUTES = {
 
 const Home2: FC = () => {
   let navigate = useNavigate()
-  const { isLoggedIn, isAuthenticated } = useAuth()
+  const {isLoggedIn, isAuthenticated} = useAuth()
 
   useEffect(() => {
     if (isAuthenticated) {
       const user = isLoggedIn()
-      navigate(`/v2/${ROUTES[user?.roles[0] || RoleType.USER_PAM]}/missions`, { replace: true })
+      let url = ULAM_V2_HOME_PATH
+      // reuse following once both pam and ulam are on v2
+      // let url = `/v2/${ROUTES[user?.roles[0] || RoleType.USER_PAM]}/missions`
+      if (user?.roles.includes(RoleType.USER_PAM)) {
+        url = PAM_HOME_PATH
+      }
+      navigate(url, {replace: true})
     } else {
-      navigate('/login', { replace: true })
+      navigate('/login', {replace: true})
     }
-  }, [isAuthenticated, navigate])
+  }, [isAuthenticated, isLoggedIn, navigate])
 
-  return <div />
+  return <div/>
 }
 
 export default Home2
