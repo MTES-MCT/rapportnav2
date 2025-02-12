67c57635cf800a03aa99a86859bd3f74ea8f6991
aleckvincent <aleck.vincent@gmail.com>
2025-02-12 16:57:09 +0100

    add Get annotation

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/api/bff/v2/ControlUnitResourceRestController.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/api/bff/v2/ControlUnitResourceRestController.kt
@@ -1,7 +1,11 @@
 package fr.gouv.dgampa.rapportnav.infrastructure.api.bff.v2
 
+import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.controlResources.ControlUnitResourceType
 import fr.gouv.dgampa.rapportnav.domain.use_cases.mission.v2.controlUnitResource.GetControlUnitResources
+import fr.gouv.dgampa.rapportnav.infrastructure.api.bff.model.v2.env.ControlUnit
 import fr.gouv.dgampa.rapportnav.infrastructure.api.bff.model.v2.env.ControlUnitResourceEnv
+import fr.gouv.dgampa.rapportnav.infrastructure.api.bff.model.v2.env.StationData
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 class ControlUnitResourceRestController(
     private val getControlUnitResources: GetControlUnitResources
 ) {
+    @GetMapping
     fun getAll(): List<ControlUnitResourceEnv>? {
         return getControlUnitResources.execute()
     }
