f335da7c5ce5a249cb7bd672bbb8ee81ae1d6478
Christian <christian.tonye@gmail.com>
2025-02-12 14:34:29 +0100

    chore(release): 2.5.2

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.lang.System.getenv
 
 group = "fr.gouv.dgampa"
-version = "2.5.1"
+version = "2.5.2"
 description = "RapportNav"
 
 val kotlinVersion by extra("1.9.24")
@@ -169,3 +169,10 @@ tasks.test {
   useJUnitPlatform()   // If you are using JUnit 5
   finalizedBy(tasks.jacocoTestReport)  // Generate the report after tests
 }
+
+flyway {
+  url = "jdbc:postgresql://localhost:5432/rapportnavdb"
+  user = "postgres"
+  password = ""
+}
+
