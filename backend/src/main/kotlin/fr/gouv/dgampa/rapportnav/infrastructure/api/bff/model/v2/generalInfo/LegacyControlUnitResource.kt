705ff003f93caa0e2803c227c5ae9df5b779b0ec
Christian <christian.tonye@gmail.com>
2025-02-27 11:18:21 +0100

    feat(frontend/backend): control unit resource send the all object
     - send the all object de resource
     - map just ids

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/api/bff/model/v2/generalInfo/LegacyControlUnitResource.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/api/bff/model/v2/generalInfo/LegacyControlUnitResource.kt
@@ -1,5 +1,7 @@
 package fr.gouv.dgampa.rapportnav.infrastructure.api.bff.model.v2.generalInfo
 
 data class LegacyControlUnitResource(
-    val id: Int
+    val id: Int,
+    val name: String,
+    val controlUnitId: Int
 )
