fdce8c5d43f93429d88501148eb6d98731519a2d
Christian <christian.tonye@gmail.com>
2025-03-17 11:08:22 +0100

    fix(frontend): tests

--- frontend/src/v2/features/common/components/layout/__tests__/mission-list-page-header-wrapper.test.tsx
+++ frontend/src/v2/features/common/components/layout/__tests__/mission-list-page-header-wrapper.test.tsx
@@ -1,9 +1,15 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../../../../../../query-client'
 import { render } from '../../../../../../test-utils'
 import MissionListPageHeaderWrapper from '../mission-list-page-header-wrapper'
 
 describe('MissionListPageHeaderWrapper', () => {
   it('should match the snapshot', () => {
-    const wrapper = render(<MissionListPageHeaderWrapper title={<>Title</>} actions={<>Actions</>} />)
+    const wrapper = render(
+      <QueryClientProvider client={queryClient}>
+        <MissionListPageHeaderWrapper title={<>Title</>} actions={<>Actions</>} />
+      </QueryClientProvider>
+    )
     expect(wrapper).toMatchSnapshot()
   })
 })
