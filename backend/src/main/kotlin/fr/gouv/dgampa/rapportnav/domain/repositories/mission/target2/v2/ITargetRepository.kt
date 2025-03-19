bc8c4669a4fde608ecf87a3cbbefcea68f50026e
Christian <christian.tonye@gmail.com>
2025-03-19 18:52:32 +0100

    feat(backend): database model target2, control2, infraction2
     - database definition target2, control2, infraction2
     - Definition  model/  target, control, infraction
     - JPA component

---
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/repositories/mission/target2/v2/ITargetRepository.kt
@@ -0,0 +1,14 @@
+package fr.gouv.dgampa.rapportnav.domain.repositories.mission.target2.v2
+
+import fr.gouv.dgampa.rapportnav.infrastructure.database.model.mission.target2.v2.TargetModel2
+import java.util.*
+
+interface ITargetRepository {
+    fun save(target: TargetModel2): TargetModel2
+
+    fun findById(id: UUID): Optional<TargetModel2>
+
+    fun findByActionId(actionId: UUID): List<TargetModel2>
+
+    fun deleteByActionId(actionId: UUID)
+}
