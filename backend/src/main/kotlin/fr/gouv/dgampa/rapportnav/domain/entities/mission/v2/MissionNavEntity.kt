4abdf600a15f7e69c562a8c21714ca03dd8c5a66
aleckvincent <aleck.vincent@gmail.com>
2025-03-31 16:56:20 +0200

    remove model mapper

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/entities/mission/v2/MissionNavEntity.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/entities/mission/v2/MissionNavEntity.kt
@@ -1,12 +1,11 @@
 package fr.gouv.dgampa.rapportnav.domain.entities.mission.v2
 
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.MissionSourceEnum
-import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.MissionTypeEnum
+import fr.gouv.dgampa.rapportnav.infrastructure.database.model.mission.MissionModel
 import java.time.Instant
 
 data class MissionNavEntity(
     val id: Int? = null,
-    val missionTypes: List<MissionTypeEnum>,
     val controlUnits: List<Int> = listOf(),
     val openBy: String? = null,
     val completedBy: String? = null,
@@ -16,4 +15,36 @@ data class MissionNavEntity(
     val missionSource: MissionSourceEnum? = MissionSourceEnum.RAPPORT_NAV,
     val observationsByUnit: String? = null,
     val controlUnitIdOwner: Int
-)
+) {
+    companion object {
+        fun fromMissionModel(model: MissionModel): MissionNavEntity {
+            return MissionNavEntity(
+                id = model.id,
+                controlUnits = model.controlUnits,
+                openBy = model.openBy,
+                completedBy = model.completedBy,
+                startDateTimeUtc = model.startDateTimeUtc,
+                endDateTimeUtc = model.endDateTimeUtc,
+                isDeleted = model.isDeleted,
+                missionSource = model.missionSource,
+                observationsByUnit = model.observationsByUnit,
+                controlUnitIdOwner = model.controlUnitIdOwner
+            )
+        }
+    }
+
+    fun toMissionModel(): MissionModel {
+        return MissionModel(
+            id = id,
+            controlUnits = controlUnits,
+            openBy = openBy,
+            completedBy = completedBy,
+            startDateTimeUtc = startDateTimeUtc,
+            endDateTimeUtc = endDateTimeUtc,
+            isDeleted = isDeleted,
+            missionSource = missionSource,
+            observationsByUnit = observationsByUnit,
+            controlUnitIdOwner = controlUnitIdOwner
+        )
+    }
+}
