198deed45b8fb42b97f5365f38752de16d14e2cd
aleckvincent <aleck.vincent@gmail.com>
2025-04-15 14:11:33 +0200

    add update mission nav

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/use_cases/mission/v2/CreateOrUpdateGeneralInfo.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/use_cases/mission/v2/CreateOrUpdateGeneralInfo.kt
@@ -4,10 +4,8 @@ import fr.gouv.dgampa.rapportnav.config.UseCase
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.nav.generalInfo.MissionGeneralInfoEntity
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.v2.MissionGeneralInfoEntity2
 import fr.gouv.dgampa.rapportnav.domain.repositories.mission.generalInfo.IMissionGeneralInfoRepository
-import fr.gouv.dgampa.rapportnav.domain.use_cases.mission.PatchEnvMission
-import fr.gouv.dgampa.rapportnav.domain.use_cases.mission.crew.AddOrUpdateMissionCrew
 import fr.gouv.dgampa.rapportnav.infrastructure.api.bff.adapters.MissionEnvInput
-import fr.gouv.dgampa.rapportnav.infrastructure.api.bff.model.crew.MissionCrew
+import fr.gouv.dgampa.rapportnav.infrastructure.api.bff.adapters.MissionNavInput
 import fr.gouv.dgampa.rapportnav.infrastructure.api.bff.model.v2.generalInfo.MissionGeneralInfo2
 import org.slf4j.LoggerFactory
 
@@ -17,11 +15,12 @@ class CreateOrUpdateGeneralInfo(
     private val updateMissionService2: UpdateMissionService2,
     private val getGeneralInfo2: GetGeneralInfo2,
     private val updateMissionEnv: UpdateMissionEnv,
-    private val processMissionCrew: ProcessMissionCrew
+    private val processMissionCrew: ProcessMissionCrew,
+    private val updateMissionNav: UpdateMissionNav
 ) {
     private val logger = LoggerFactory.getLogger(CreateOrUpdateGeneralInfo::class.java)
 
-    fun execute(missionId: Int, generalInfo: MissionGeneralInfo2): MissionGeneralInfoEntity2? {
+    fun execute(missionId: String, generalInfo: MissionGeneralInfo2): MissionGeneralInfoEntity2? {
 
         try {
             val entityToSave = generalInfo.toMissionGeneralInfoEntity(missionId)
@@ -43,17 +42,29 @@ class CreateOrUpdateGeneralInfo(
                 )
             }
 
-            // patch MonitorEnv fields through API
-            updateMissionEnv.execute(
-                input = MissionEnvInput(
-                    missionId = missionId,
-                    startDateTimeUtc = generalInfo.startDateTimeUtc,
-                    endDateTimeUtc = generalInfo.endDateTimeUtc,
-                    missionTypes = generalInfo.missionTypes,
-                    observationsByUnit = generalInfo.observations,
-                    resources = generalInfo.resources?.map { it.toLegacyControlUnitResourceEntity() },
+            if (generalInfo.isMissionNav()) {
+                updateMissionNav.execute(
+                    input = MissionNavInput(
+                        id = missionId.toString(),
+                        startDateTimeUtc = generalInfo.startDateTimeUtc,
+                        endDateTimeUtc = generalInfo.endDateTimeUtc,
+                        observationsByUnit = generalInfo.observations
+                    )
                 )
-            )
+
+            } else {
+                // patch MonitorEnv fields through API
+                updateMissionEnv.execute(
+                    input = MissionEnvInput(
+                        missionId = missionId,
+                        startDateTimeUtc = generalInfo.startDateTimeUtc,
+                        endDateTimeUtc = generalInfo.endDateTimeUtc,
+                        missionTypes = generalInfo.missionTypes,
+                        observationsByUnit = generalInfo.observations,
+                        resources = generalInfo.resources?.map { it.toLegacyControlUnitResourceEntity() },
+                    )
+                )
+            }
 
             val generalInfoEntity = MissionGeneralInfoEntity.fromMissionGeneralInfoModel(generalInfoModel)
             return MissionGeneralInfoEntity2(data = generalInfoEntity)
--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/database/repositories/interfaces/mission/IDBMissionRepository.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/database/repositories/interfaces/mission/IDBMissionRepository.kt
@@ -3,9 +3,23 @@ package fr.gouv.dgampa.rapportnav.infrastructure.database.repositories.interface
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.v2.MissionNavEntity
 import fr.gouv.dgampa.rapportnav.infrastructure.database.model.mission.MissionModel
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.time.Instant
 import java.util.*
 
 interface IDBMissionRepository: JpaRepository<MissionModel, UUID> {
 
     fun save(entity: MissionNavEntity): MissionModel
+
+    @Query("""
+    SELECT m FROM MissionModel m
+    WHERE m.startDateTimeUtc >= :startBeforeDateTime
+    AND m.endDateTimeUtc <= :endBeforeDateTime
+    ORDER BY m.startDateTimeUtc DESC
+    """)
+    fun findAllBetweenDates(
+        @Param("startBeforeDateTime") startBeforeDateTime: Instant,
+        @Param("endBeforeDateTime") endBeforeDateTime: Instant
+    ): List<MissionModel?>
 }
