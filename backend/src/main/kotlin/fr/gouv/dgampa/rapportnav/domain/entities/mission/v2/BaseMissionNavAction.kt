7e915fbb91ea0e4c9d679b7b8ad2c03ed43becff
aleckvincent <aleck.vincent@gmail.com>
2025-04-30 10:45:56 +0200

    add mission_id_string for MissionAction

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/entities/mission/v2/BaseMissionNavAction.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/entities/mission/v2/BaseMissionNavAction.kt
@@ -12,6 +12,7 @@ import java.util.*
 interface BaseMissionNavAction {
     var id: UUID
     var missionId: Int
+    var missionIdString: String?
     var actionType: ActionType
     var isCompleteForStats: Boolean?
     val sourcesOfMissingDataForStats: List<MissionSourceEnum>?
