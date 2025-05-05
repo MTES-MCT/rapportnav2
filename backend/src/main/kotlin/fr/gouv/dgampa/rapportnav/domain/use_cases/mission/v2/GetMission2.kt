e6674dbf62efca6a482174f71476335dc9eb1b03
aleckvincent <aleck.vincent@gmail.com>
2025-05-05 15:03:04 +0200

    fix

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/use_cases/mission/v2/GetMission2.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/domain/use_cases/mission/v2/GetMission2.kt
@@ -5,6 +5,7 @@ import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.MissionEntity
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.v2.MissionEntity2
 import fr.gouv.dgampa.rapportnav.domain.use_cases.mission.action.v2.GetEnvMissionById2
 import fr.gouv.dgampa.rapportnav.domain.use_cases.mission.action.v2.GetMissionAction
+import fr.gouv.dgampa.rapportnav.domain.utils.isValidUUID
 
 @UseCase
 class GetMission2(
@@ -14,11 +15,24 @@ class GetMission2(
     private val getNavMissionById2: GetNavMissionById2
 ) {
     fun execute(missionId: String? = null, envMission: MissionEntity? = null): MissionEntity2? {
-        val mission = envMission ?: missionId?.let { getEnvMissionById2.execute(it.toInt()) ?: getNavMissionById2.execute(it) } ?: return null
+        val mission = envMission
+            ?: missionId?.let {
+                if (isValidUUID(it)) {
+                    getNavMissionById2.execute(it)
+                } else {
+                    it.toIntOrNull()?.let { id -> getEnvMissionById2.execute(id) }
+                }
+            } ?: return null
 
         val id = if (mission.missionIdString !== null) mission.missionIdString else if (mission.id !== null) mission.id.toString() else return null
 
-        val actions = getMissionAction.execute(missionId = id.toInt())
+
+        val actions = if (!isValidUUID(id)) {
+            getMissionAction.execute(missionId = id.toInt())
+        } else {
+            listOf()
+        }
+
         val generalInfos = getGeneralInfos2.execute(missionId = id, controlUnits = mission.controlUnits)
 
         return MissionEntity2(
