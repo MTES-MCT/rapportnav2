21685165e593ab7cd53c7b106a7d1c93a0ff3cb3
Christian <christian.tonye@gmail.com>
2025-05-14 16:01:07 +0200

    feat(backend):  stubs fix, fishactions, add vessel
     - wiremock vessel add

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/api/bff/wiremock/FishVesselsStubs.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/api/bff/wiremock/FishVesselsStubs.kt
@@ -4,14 +4,13 @@ import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import fr.gouv.dgampa.rapportnav.infrastructure.api.bff.wiremock.utils.LoadJsonData
 
-object MissionByIdStubs {
+object FishVesselsStubs {
 
     fun configureStubs(wireMockServer: WireMockServer) {
-
-        val json = LoadJsonData.load("env/mission.json")
+        val json = LoadJsonData.load("fish/vessels.json")
 
         wireMockServer.stubFor(
-            WireMock.get(WireMock.urlMatching("/api/v1/missions/\\d+"))
+            WireMock.get(WireMock.urlMatching("/api/v1/vessels"))
                 .willReturn(
                     WireMock.aResponse()
                         .withHeader("Content-Type", "application/json")
