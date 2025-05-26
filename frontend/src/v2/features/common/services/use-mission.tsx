ce76d481cc75a2113d08e394eec5b10aa003bec3
lwih <louis.hache@beta.gouv.fr>
2025-05-26 16:55:48 +0200

    feat: add "last sync at" message in footer

--- frontend/src/v2/features/common/services/use-mission.tsx
+++ frontend/src/v2/features/common/services/use-mission.tsx
@@ -1,4 +1,4 @@
-import { skipToken, useQuery } from '@tanstack/react-query'
+import { skipToken, useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from '../../../../query-client/axios'
 import { Mission2 } from '../types/mission-types'
 import { missionsKeys } from './query-keys.ts'
@@ -9,11 +9,14 @@ export const fetchMission = (missionId: string): Promise<Mission2> =>
     return response.data
   })
 const useGetMissionQuery = (missionId?: string) => {
+  const queryClient = useQueryClient()
+
   const query = useQuery<Mission2>({
     queryKey: missionsKeys.byId(missionId),
     queryFn: missionId ? () => fetchMission(missionId) : skipToken,
     staleTime: DYNAMIC_DATA_STALE_TIME, // Cache data for 5 minutes
-    retry: 2 // Retry failed requests twice before throwing an error
+    retry: 2, // Retry failed requests twice before throwing an error
+    initialDataUpdatedAt: () => queryClient.getQueryState(missionsKeys.byId(missionId))?.dataUpdatedAt // used in footer to show "last sync at xxx" message
   })
 
   return query
