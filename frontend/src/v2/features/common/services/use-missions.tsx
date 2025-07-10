50a388481387eab92edb6d2f3028911eab412c7e
Christian <christian.tonye@gmail.com>
2025-07-10 17:27:06 +0200

    feat(frontend): handle searchParams to mission list
     - Add search param to url for mission list
     - Update useDate to get range date from date on month and year
     - test it

--- frontend/src/v2/features/common/services/use-missions.tsx
+++ frontend/src/v2/features/common/services/use-missions.tsx
@@ -1,36 +1,27 @@
 import { useQuery, useQueryClient, UseQueryResult } from '@tanstack/react-query'
+import { useEffect } from 'react'
+import { DYNAMIC_DATA_STALE_TIME } from '../../../../query-client'
 import axios from '../../../../query-client/axios.ts'
 import { Mission2 } from '../types/mission-types.ts'
 import { missionsKeys } from './query-keys.ts'
-import { useEffect } from 'react'
-import { DYNAMIC_DATA_STALE_TIME } from '../../../../query-client'
 
-interface UseMissionsQueryParams {
-  startDateTimeUtc: string
-  endDateTimeUtc?: string
-}
-
-const useMissionsQuery = ({
-  startDateTimeUtc,
-  endDateTimeUtc
-}: UseMissionsQueryParams): UseQueryResult<Mission2[], Error> => {
+const useMissionsQuery = (params: URLSearchParams): UseQueryResult<Mission2[], Error> => {
   const queryClient = useQueryClient()
 
   const fetchMissions = async (): Promise<Mission2[]> => {
-    const params = new URLSearchParams({ startDateTimeUtc })
-    if (endDateTimeUtc) {
-      params.append('endDateTimeUtc', endDateTimeUtc)
-    }
     const response = await axios.get<Mission2[]>(`missions?${params.toString()}`)
     return response.data
   }
 
+  const endDateTimeUtc = params.get('endDateTimeUtc')
+  const startDateTimeUtc = params.get('startDateTimeUtc')
+
   const query = useQuery<Mission2[], Error>({
     queryKey: missionsKeys.filter(JSON.stringify({ startDateTimeUtc, endDateTimeUtc })),
     queryFn: fetchMissions,
-    enabled: !!startDateTimeUtc, // Prevents query from running if startDateTimeUtc is not provided
+    enabled: !!endDateTimeUtc && !!startDateTimeUtc, // Prevents query from running if startDateTimeUtc is not provided
     staleTime: DYNAMIC_DATA_STALE_TIME, // Cache data for 5 minutes
-    retry: 2 // Retry failed requests twice before throwing an error
+    retry: 2 // Retry failed requests twice before throwing an error,
   })
 
   useEffect(() => {
