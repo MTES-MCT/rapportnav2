978effc44d3ea117349f6f5a95ad89f82a2a0f69
lwih <louis.hache@beta.gouv.fr>
2025-07-15 16:42:09 +0200

    fix: try fix boat icons and fix alt text

--- frontend/src/features/common/components/ui/icon/IconVesselFishing.tsx
+++ frontend/src/features/common/components/ui/icon/IconVesselFishing.tsx
@@ -1,7 +1,7 @@
 import Icon from '../../../../../assets/images/navire_peche_pro.svg'
 
 function IconVesselFishing() {
-  return <img src={Icon} alt="Navire de commerce" />
+  return <img src={Icon} alt="Navire de pêche professionnelle" height={32} />
 }
 
 export default IconVesselFishing
