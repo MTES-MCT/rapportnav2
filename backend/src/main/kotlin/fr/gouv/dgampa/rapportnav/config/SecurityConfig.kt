9c80d08ad56270fc2ccfa480216bfd1323045a3d
lwih <louis.hache@beta.gouv.fr>
2025-07-15 10:45:57 +0200

    fix: allow non admin for register endpoint

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/config/SecurityConfig.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/config/SecurityConfig.kt
@@ -63,7 +63,8 @@ class SecurityConfig(
             authorize
                 .requestMatchers("/graphql").authenticated()
                 .requestMatchers("/api/v1/auth/login").permitAll()
-                .requestMatchers("/api/v1/auth/register").hasAuthority(AuthoritiesEnum.ROLE_ADMIN.toString())
+                .requestMatchers("/api/v1/auth/register").permitAll()
+//                .requestMatchers("/api/v1/auth/register").hasAuthority(AuthoritiesEnum.ROLE_ADMIN.toString())
                 .requestMatchers("/api/v1/admin/**").hasAuthority(AuthoritiesEnum.ROLE_ADMIN.toString())
                 .requestMatchers("/api/v2/**").authenticated()
                 .requestMatchers("/**").permitAll()
