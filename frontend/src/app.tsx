99d7fe9d89c7080c280cdb16d937a10703692143
Louis H <4593884+lwih@users.noreply.github.com>
2025-09-11 11:32:36 +0200

    feat: bump to Sentry10 (#853)

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -5,23 +5,11 @@ import { CachePersistor, LocalStorageWrapper } from 'apollo3-cache-persist'
 import { FC, useEffect, useState } from 'react'
 import apolloClient, { apolloCache } from './apollo-client/apollo-client.ts'
 import UIThemeWrapper from './features/common/components/ui/ui-theme-wrapper'
-import ErrorPage from './pages/error-page.tsx'
 import queryClient, { persistOptions } from './query-client'
 import { router } from './router/router'
 import RouterProvider from './router/router-provider'
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-// import { FlagProvider } from '@unleash/proxy-client-react'
-// import { IConfig } from 'unleash-proxy-client'
-
-// const config: IConfig = {
-//   url: 'http://localhost:3000/proxy', // Your front-end API URL or the Unleash proxy's URL (https://<proxy-url>/proxy)
-//   clientKey: 'randomkey', // A client-side API token OR one of your proxy's designated client keys (previously known as proxy secrets)
-//   refreshInterval: 60, // How often (in seconds) the client should poll the proxy for updates
-//   appName: 'rapportnav2-flags', // The name of your application. It's only used for identifying your application
-//   environment: 'local'
-// }
 
 const App: FC = () => {
   const [loading, setLoading] = useState(true)
@@ -53,30 +41,26 @@ const App: FC = () => {
   }
 
   return (
-    <Sentry.ErrorBoundary fallback={ErrorPage}>
-      <ApolloProvider client={client}>
-        <UIThemeWrapper>
-          <PersistQueryClientProvider //
-            client={queryClient}
-            persistOptions={persistOptions}
-            onSuccess={() => {
-              queryClient.resumePausedMutations().then(() => {
-                console.log('Persistence restored successfully')
-              })
-            }}
-            onError={() => {
-              console.log('Persistence restored failed')
-            }}
-          >
-            {/*<FlagProvider config={config}>*/}
-            <Notifier />
-            <RouterProvider router={router} />
-            {/*</FlagProvider>*/}
-            <ReactQueryDevtools initialIsOpen={false} />
-          </PersistQueryClientProvider>
-        </UIThemeWrapper>
-      </ApolloProvider>
-    </Sentry.ErrorBoundary>
+    <ApolloProvider client={client}>
+      <UIThemeWrapper>
+        <PersistQueryClientProvider //
+          client={queryClient}
+          persistOptions={persistOptions}
+          onSuccess={() => {
+            queryClient.resumePausedMutations().then(() => {
+              console.log('Persistence restored successfully')
+            })
+          }}
+          onError={() => {
+            console.log('Persistence restored failed')
+          }}
+        >
+          <Notifier />
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </PersistQueryClientProvider>
+      </UIThemeWrapper>
+    </ApolloProvider>
   )
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,9 @@
+// Sentry initialization should be imported first!
+import { initializeSentry } from './sentry'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import reportWebVitals from './report-web-vitals'
 import App from './app'
-import initSentry from './sentry.ts'
 
 import 'react-toastify/dist/ReactToastify.css'
 import 'rsuite/dist/rsuite.min.css'
@@ -11,18 +12,24 @@ import '@mtes-mct/monitor-ui/assets/stylesheets/rsuite-override.css'
 import { setDefaultOptions } from 'date-fns'
 import { fr } from 'date-fns/locale'
 
-// setup sentry before starting
-initSentry()
-
 // setup dates in French
 setDefaultOptions({ locale: fr })
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+initializeSentry().then(Sentry => {
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement, {
+    onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+      console.warn('Uncaught error', error, errorInfo.componentStack)
+    }),
+    onCaughtError: Sentry.reactErrorHandler(),
+    onRecoverableError: Sentry.reactErrorHandler()
+  })
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
