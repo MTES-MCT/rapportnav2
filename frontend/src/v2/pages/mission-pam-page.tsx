5bb55ecf102ecafd3dd43dbfb3721b2a681c9436
lwih <louis.hache@beta.gouv.fr>
2025-09-30 18:07:34 +0200

    fix: cache synchro across pages

--- frontend/src/v2/pages/mission-pam-page.tsx
+++ frontend/src/v2/pages/mission-pam-page.tsx
@@ -1,6 +1,6 @@
 import { useGlobalRoutes } from '@router/use-global-routes.tsx'
 import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import PageWrapper from '../features/common/components/layout/page-wrapper.tsx'
 import MissionPageFooter from '../features/common/components/ui/mission-page-footer.tsx'
 import { OwnerType } from '../features/common/types/owner-type.ts'
@@ -9,12 +9,14 @@ import MissionActionPam from '../features/pam/components/element/mission-action-
 import MissionHeaderPam from '../features/pam/components/element/mission-header-pam.tsx'
 import MissionTimelinePam from '../features/pam/components/element/mission-timeline-pam.tsx'
 import OfflineDialog from '../features/pam/components/ui/offline-dialog.tsx'
+import { missionsKeys } from '../features/common/services/query-keys.ts'
+import useAuth from '../features/auth/hooks/use-auth.tsx'
 
 const MissionPamPage: React.FC = () => {
   const { getUrl } = useGlobalRoutes()
   let { missionId, actionId } = useParams()
-  const navigate = useNavigate()
-  const exitMission = async () => navigate(getUrl(OwnerType.MISSION))
+  const { navigateAndResetCache } = useAuth()
+  const exitMission = async () => navigateAndResetCache(getUrl(OwnerType.MISSION), missionsKeys.byId(missionId))
 
   return (
     <>
