686e727f81fa252d3bac0143a93746492e527943
lwih <louis.hache@beta.gouv.fr>
2025-10-23 15:29:14 +0200

    fix(db): fix amount of controls

---
+++ backend/src/main/resources/db/migration/V1.2025.10.23.17.30__fix_amount_of_controls.sql
@@ -0,0 +1,9 @@
+
+UPDATE control_2
+SET amount_of_controls = 10
+WHERE id IN (
+   '177114d8-d82b-477c-a2d8-50429d205882',
+   'bc7642e0-1f38-485f-92a1-bced2b1c35ec',
+   '06fa33a7-b0c8-48e0-bcec-d49a134afab8'
+);
+
