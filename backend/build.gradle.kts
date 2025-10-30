c64d5b0d590cb9c60a39c1b85bb4a43c89e1594e
lwih <louis.hache@beta.gouv.fr>
2025-10-30 11:11:25 +0100

    fix(CI): exclude CVE

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -90,7 +90,7 @@ dependencies {
   implementation("org.apache.commons:commons-text:1.14.0")
   implementation("org.jodconverter:jodconverter-local-lo:4.4.7")
   implementation("com.neovisionaries:nv-i18n:1.29")
-  implementation("org.wiremock:wiremock-standalone:3.13.1")
+  implementation("org.wiremock:wiremock-standalone:4.0.0-beta.15")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
   testImplementation("org.springframework.boot:spring-boot-starter-test:$springVersion")
   testImplementation("org.springframework:spring-webflux:6.2.12")
