1f495fe8431cffd52272512e2882ad2c3470fefd
Christian <christian.tonye@gmail.com>
2025-11-04 16:05:11 +0100

    feature(backend): secure config
     - Change admin v1 to v2

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/config/SecurityConfig.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/config/SecurityConfig.kt
@@ -81,7 +81,7 @@ class SecurityConfig(
                 .requestMatchers("/graphql").authenticated()
                 .requestMatchers("/api/v1/auth/login").permitAll()
                 .requestMatchers("/api/v1/auth/register").hasAuthority(AuthoritiesEnum.ROLE_ADMIN.toString())
-                .requestMatchers("/api/v1/admin/**").hasAuthority(AuthoritiesEnum.ROLE_ADMIN.toString())
+                .requestMatchers("/api/v2/admin/**").hasAuthority(AuthoritiesEnum.ROLE_ADMIN.toString())
                 .requestMatchers("/api/v2/**").authenticated()
                 .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated()
