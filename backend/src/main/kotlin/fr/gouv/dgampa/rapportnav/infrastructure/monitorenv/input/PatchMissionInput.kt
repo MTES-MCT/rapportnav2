c1179fc6255905c08013d49fceab7573dcaa8bdd
Christian <christian.tonye@gmail.com>
2025-11-05 22:58:26 +0100

    feat(backend):  resources in patch mission env
     - Update process of patch
     - mission env input add transform method
     - update test usecase, model

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/monitorenv/input/PatchMissionInput.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/monitorenv/input/PatchMissionInput.kt
@@ -3,10 +3,8 @@ package fr.gouv.dgampa.rapportnav.infrastructure.monitorenv.input
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.MissionEntity
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.MissionTypeEnum
 import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.controlResources.LegacyControlUnitEntity
-import fr.gouv.dgampa.rapportnav.domain.entities.mission.env.controlResources.LegacyControlUnitResourceEntity
 import java.time.Instant
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -17,30 +15,4 @@ data class PatchMissionInput @JsonCreator(mode = JsonCreator.Mode.PROPERTIES) co
     @JsonProperty("endDateTimeUtc") val endDateTimeUtc: Instant? = null,
     @JsonProperty("isUnderJdp") val isUnderJdp: Boolean? = null,
     @JsonProperty("observationsByUnit") val observationsByUnit: String? = null
-) {
-
-    fun withControlUnits(
-        resources: List<LegacyControlUnitResourceEntity>?,
-        controlUnits: List<LegacyControlUnitEntity>
-    ): PatchMissionInput {
-        val controlUnitId = resources?.firstOrNull()?.controlUnitId
-        this.controlUnits = controlUnits.map { controlUnit ->
-            controlUnit.takeIf { it.id != controlUnitId } ?: controlUnit.copy(
-                resources = resources?.toMutableList() ?: controlUnit.resources?.toMutableList()
-            )
-        }
-        return this
-    }
-
-    fun hasNotChanged(other: MissionEntity?): Boolean {
-        if (other == null) return false
-        if (isUnderJdp != other.isUnderJdp) return false
-        if (missionTypes != other.missionTypes) return false
-        if (startDateTimeUtc != other.startDateTimeUtc) return false
-        if (endDateTimeUtc != other.endDateTimeUtc) return false
-        if (observationsByUnit != other.observationsByUnit) return false
-        if (controlUnits?.size != other.controlUnits.size) return false
-        if (controlUnits?.toSet() != other.controlUnits.toSet()) return false
-        return true
-    }
-}
+)
