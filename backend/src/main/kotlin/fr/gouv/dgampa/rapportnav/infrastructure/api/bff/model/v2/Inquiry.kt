91c7950227f3824def55c9a81d1d862b193356e6
Christian <christian.tonye@gmail.com>
2025-11-27 15:42:50 +0100

    feat(backend): Vessel search on request
     - Add search param for the vessel api
     - For Inquiry, return the all vessel object instead of just the vesselId
     - Update test

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/api/bff/model/v2/Inquiry.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/api/bff/model/v2/Inquiry.kt
@@ -7,7 +7,6 @@ import java.util.*
 class Inquiry(
     var id: UUID? = null,
     val agentId: Int? = null,
-    val vesselId: Int? = null,
     val serviceId: Int? = null,
     var endDateTimeUtc: Instant? = null,
     val startDateTimeUtc: Instant? = null,
@@ -15,13 +14,12 @@ class Inquiry(
     val origin: InquiryOriginType? = null,
     val status: InquiryStatusType? = null,
     val conclusion: InquiryConclusionType? = null,
-    val vesselName: String? = null,
-    val vesselExternalReferenceNumber: String? = null,
     val missionId: Int? = null,
     val missionIdUUID: UUID? = null,
     var actions: List<MissionNavAction?> = listOf(),
     var isSignedByInspector: Boolean? = null,
-    var siren: String? = null
+    var siren: String? = null,
+    val vessel: Vessel?= null
 ) {
 
     fun toInquiryEntity(): InquiryEntity {
@@ -31,18 +29,17 @@ class Inquiry(
             status = status,
             origin = origin,
             agentId = agentId,
-            vesselId = vesselId,
             serviceId = serviceId,
             conclusion = conclusion,
-            vesselName = vesselName,
             endDateTimeUtc = endDateTimeUtc,
             startDateTimeUtc = startDateTimeUtc,
             missionId = missionId,
             missionIdUUID = missionIdUUID,
             isSignedByInspector = isSignedByInspector,
-            vesselExternalReferenceNumber = vesselExternalReferenceNumber,
             siren = siren,
-            actions = listOf()
+            actions = listOf(),
+            vessel = vessel?.toVesselEntity()
+
         )
     }
 
@@ -54,17 +51,15 @@ class Inquiry(
                 status = entity?.status,
                 origin = entity?.origin,
                 agentId = entity?.agentId,
-                vesselId = entity?.vesselId,
                 serviceId = entity?.serviceId,
                 conclusion = entity?.conclusion,
-                vesselName = entity?.vesselName,
                 endDateTimeUtc = entity?.endDateTimeUtc,
                 startDateTimeUtc = entity?.startDateTimeUtc,
                 missionId = entity?.missionId,
                 missionIdUUID = entity?.missionIdUUID,
                 isSignedByInspector = entity?.isSignedByInspector,
-                vesselExternalReferenceNumber = entity?.vesselExternalReferenceNumber,
                 siren = entity?.siren,
+                vessel = entity?.vessel?.let { Vessel.fromVesselEntity(it) },
                 actions = entity?.actions?.map { action -> MissionNavAction.fromMissionActionEntity(action) } ?: listOf()
             )
         }
