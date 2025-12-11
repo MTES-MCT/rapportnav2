4ef16a56d6155ec5cac15bd9b1602ea27a2aedc6
lwih <louis.hache@beta.gouv.fr>
2025-12-11 17:55:32 +0100

    fix(offline): fix setting individual cache keys causing memory leaks

--- frontend/src/v2/features/common/services/use-mission.tsx
+++ frontend/src/v2/features/common/services/use-mission.tsx
@@ -1,9 +1,10 @@
-import { skipToken, useQuery } from '@tanstack/react-query'
+import { skipToken, useQuery, useQueryClient } from '@tanstack/react-query'
 import { DYNAMIC_DATA_STALE_TIME } from '../../../../query-client'
 import axios from '../../../../query-client/axios'
 import { Mission2 } from '../types/mission-types'
-import { missionsKeys } from './query-keys.ts'
+import { actionsKeys, missionsKeys } from './query-keys.ts'
 import { useOnlineManager } from '../hooks/use-online-manager.tsx'
+import { MissionAction } from '../types/mission-action.ts'
 
 export const fetchMission = (missionId?: string): Promise<Mission2> =>
   axios.get(`missions/${missionId}`).then(response => {
@@ -11,18 +12,26 @@ export const fetchMission = (missionId?: string): Promise<Mission2> =>
   })
 
 const useGetMissionQuery = (missionId?: string) => {
+  const queryClient = useQueryClient()
   const { isOnline } = useOnlineManager()
 
-  const query = useQuery<Mission2>({
+  return useQuery<Mission2>({
     queryKey: missionsKeys.byId(missionId),
-    queryFn: missionId ? () => fetchMission(missionId) : skipToken,
+    queryFn: missionId
+      ? async () => {
+          const mission = await fetchMission(missionId)
+          // prefill individual cache keys
+          mission.actions?.forEach((action: MissionAction) => {
+            queryClient.setQueryData(actionsKeys.byId(action.id), action)
+          })
+          return mission
+        }
+      : skipToken,
     staleTime: DYNAMIC_DATA_STALE_TIME, // Cache data for 5 minutes
     retry: 2, // Retry failed requests twice before throwing an error
     refetchInterval: DYNAMIC_DATA_STALE_TIME,
     enabled: isOnline
   })
-
-  return query
 }
 
 export default useGetMissionQuery
