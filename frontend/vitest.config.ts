f8df964dc9ea8ad971071ec52cf6c2c7847397a4
lwih <louis.hache@beta.gouv.fr>
2026-03-03 12:41:32 +0100

    chore(dependencies): upgrade frontend dependencies, most notably vitest

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -9,6 +9,7 @@ export default mergeConfig(
       globals: true,
       environment: 'jsdom',
       setupFiles: ['./vitest.setup.ts'],
+      testTimeout: 10000,
       server: {
         deps: {
           inline: ['@mtes-mct/monitor-ui', 'styled-components']
