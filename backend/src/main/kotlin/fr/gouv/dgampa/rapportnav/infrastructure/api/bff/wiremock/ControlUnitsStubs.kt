bdde18553fe8d69549e72905555401f1850b58ee
lwih <louis.hache@beta.gouv.fr>
2026-03-12 12:35:01 +0100

    fix: add controlUnits mocks

---
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/api/bff/wiremock/ControlUnitsStubs.kt
@@ -0,0 +1,21 @@
+package fr.gouv.dgampa.rapportnav.infrastructure.api.bff.wiremock
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import fr.gouv.dgampa.rapportnav.infrastructure.api.bff.wiremock.utils.LoadJsonData
+
+object ControlUnitsStubs {
+    fun configureStubs(wireMockServer: WireMockServer) {
+
+        val json = LoadJsonData.load("env/control_units.json")
+
+        wireMockServer.stubFor(
+            WireMock.get(WireMock.urlPathEqualTo("/api/v1/control_units"))
+                .willReturn(
+                    WireMock.aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(json)
+                )
+        )
+    }
+}
