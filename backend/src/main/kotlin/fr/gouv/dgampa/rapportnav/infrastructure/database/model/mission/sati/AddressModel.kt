f778dac17ef1f41851d1307a2420a9e1a1d4ca22
Christian <christian.tonye@gmail.com>
2026-03-30 11:23:56 +0200

    feat(backend): add listener on model

--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/database/model/mission/sati/AddressModel.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/database/model/mission/sati/AddressModel.kt
@@ -2,14 +2,18 @@ package fr.gouv.dgampa.rapportnav.infrastructure.database.model.mission.sati
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 import java.util.UUID
 
 @Entity
 @Table(name = "address")
+@EntityListeners(AuditingEntityListener::class)
 data class AddressModel(
 
     @Id
@@ -29,6 +33,7 @@ data class AddressModel(
     @Column(length = 3)
     val country: String? = null,
 
+    @CreatedDate
     @Column(name = "created_at", nullable = false)
     val createdAt: Instant? = null
 )
--- backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/database/model/mission/sati/ContactModel.kt
+++ backend/src/main/kotlin/fr/gouv/dgampa/rapportnav/infrastructure/database/model/mission/sati/ContactModel.kt
@@ -2,17 +2,21 @@ package fr.gouv.dgampa.rapportnav.infrastructure.database.model.mission.sati
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 import java.util.UUID
 
 @Entity
 @Table(name = "contact")
+@EntityListeners(AuditingEntityListener::class)
 data class ContactModel(
 
     @Id
@@ -36,6 +40,7 @@ data class ContactModel(
     @JoinColumn(name = "address_id", referencedColumnName = "id")
     val address: AddressModel? = null,
 
+    @CreatedDate
     @Column(name = "created_at", nullable = false)
     val createdAt: Instant? = null
 )
