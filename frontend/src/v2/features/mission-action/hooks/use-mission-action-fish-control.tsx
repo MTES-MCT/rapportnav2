e862bbfcd823d10ba202b97f4034b2315aa18e0a
Christian <christian.tonye@gmail.com>
2026-06-11 15:15:54 +0200

    fix(frontend): rebase issues

--- frontend/src/v2/features/mission-action/hooks/use-mission-action-fish-control.tsx
+++ frontend/src/v2/features/mission-action/hooks/use-mission-action-fish-control.tsx
@@ -1,19 +1,18 @@
+import { useMemo } from 'react'
+import { object, string } from 'yup'
 import { StyledTabItem } from '../../common/components/ui/styled-tab.tsx'
 import { useAbstractFormik } from '../../common/hooks/use-abstract-formik-form'
 import { useCoordinate } from '../../common/hooks/use-coordinate'
 import { useDate } from '../../common/hooks/use-date'
+import { useMissionDates } from '../../common/hooks/use-mission-dates.tsx'
 import { useMissionFinished } from '../../common/hooks/use-mission-finished.tsx'
+import getDateRangeSchema from '../../common/schemas/dates-schema.ts'
 import { AbstractFormikSubFormHook } from '../../common/types/abstract-formik-hook'
 import { MissionAction, MissionFishActionData } from '../../common/types/mission-action'
 import FishControlConclusion from '../components/ui/fish-control-conclusion.tsx'
 import FishControlOthers from '../components/ui/fish-control-others.tsx'
 import FishControlPolpeche from '../components/ui/fish-control-polpeche.tsx'
 import { ActionFishControlInput } from '../types/action-type'
-import { useMissionDates } from '../../common/hooks/use-mission-dates.tsx'
-import { object, string } from 'yup'
-import getDateRangeSchema from '../../common/schemas/dates-schema.ts'
-import { useMemo } from 'react'
-import { useMissionFinished } from '../../common/hooks/use-mission-finished.tsx'
 
 const ITEMS: StyledTabItem[] = [
   {
@@ -32,7 +31,6 @@ const ITEMS: StyledTabItem[] = [
     component: FishControlConclusion
   }
 ]
-
 
 export function useMissionActionFishControl(
   action: MissionAction,
@@ -88,6 +86,7 @@ export function useMissionActionFishControl(
 
   return {
     initValue,
+    items: ITEMS,
     validationSchema,
     handleSubmit: handleSubmitOverride
   }
