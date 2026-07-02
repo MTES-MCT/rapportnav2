eeb4d20aa58dd4fb6727a38ada2a06c1ba9b3edd
Christian <christian.tonye@gmail.com>
2026-07-02 23:47:33 +0200

    feat(frontend): feature flag
     - Add feature flag to enable sati

--- frontend/src/v2/features/fish-sati/components/elements/fish-control-conclusion.tsx
+++ frontend/src/v2/features/fish-sati/components/elements/fish-control-conclusion.tsx
@@ -11,6 +11,7 @@ import FishControlSeizureSection from '../../../mission-control/components/ui/fi
 import InfractionFishSummary from '../../../mission-infraction/components/ui/infraction-fish-summary.tsx'
 import TargetItemDefault from '../../../mission-target/components/elements/target-item-default.tsx'
 import { useTarget } from '../../../mission-target/hooks/use-target.tsx'
+import { useSati } from '../../hooks/use-sati.tsx'
 import InspectorList from '../ui/inpector-list.tsx'
 import InspectorPrincipal from '../ui/inspector-item-principal.tsx'
 import ResourceForm from '../ui/resource-form.tsx'
@@ -21,30 +22,37 @@ interface FishControlConclusionProps {
 
 const FishControlConclusion: FC<FishControlConclusionProps> = ({ values }) => {
   const showCheckbox = false // TODO: remove when MonitorFish dev is done
+  const { isSatiEnabled } = useSati()
   const { defaultControlTypes } = useTarget()
   const { setFieldValue } = useFormikContext<ActionFishControlInput>()
 
   return (
     <Stack direction="column" spacing="0.5rem" alignItems="flex-start" style={{ width: '100%' }}>
       <Stack.Item style={{ width: '100%' }}>
-        <ResourceForm
-          resource={values?.sati?.resource}
-          onChange={resource => setFieldValue('sati.resource', resource)}
-        />
-      </Stack.Item>
-      <Stack.Item style={{ width: '100%' }}>
-        <InspectorPrincipal
-          inspector={values?.sati?.principalInspector}
-          onChange={inspector => setFieldValue('sati.principalInspector', inspector)}
-        />
-      </Stack.Item>
+        {isSatiEnabled && (
+          <Stack direction="column" spacing="0.5rem" alignItems="flex-start" style={{ width: '100%' }}>
+            <Stack.Item style={{ width: '100%' }}>
+              <ResourceForm
+                resource={values?.sati?.resource}
+                onChange={resource => setFieldValue('sati.resource', resource)}
+              />
+            </Stack.Item>
+            <Stack.Item style={{ width: '100%' }}>
+              <InspectorPrincipal
+                inspector={values?.sati?.principalInspector}
+                onChange={inspector => setFieldValue('sati.principalInspector', inspector)}
+              />
+            </Stack.Item>
 
-      <Stack.Item style={{ width: '100%' }}>
-        <FieldArray name="sati.otherInspectors">
-          {(fieldArray: FieldArrayRenderProps) => (
-            <InspectorList fieldArray={fieldArray} inspectors={values?.sati?.otherInspectors} />
-          )}
-        </FieldArray>
+            <Stack.Item style={{ width: '100%' }}>
+              <FieldArray name="sati.otherInspectors">
+                {(fieldArray: FieldArrayRenderProps) => (
+                  <InspectorList fieldArray={fieldArray} inspectors={values?.sati?.otherInspectors} />
+                )}
+              </FieldArray>
+            </Stack.Item>
+          </Stack>
+        )}
       </Stack.Item>
 
       <Stack.Item style={{ width: '100%' }}>
@@ -80,38 +88,55 @@ const FishControlConclusion: FC<FishControlConclusionProps> = ({ values }) => {
           <Stack.Item style={{ width: '100%' }}>
             <FishControlSeizureSection action={values} />
           </Stack.Item>
+          {!isSatiEnabled && (
+            <Stack.Item style={{ width: '100%' }}>
+              <FormikTextAreaInput
+                isLight={false}
+                name="observationsByUnit"
+                data-testid="observations-by-unit"
+                label="Observation de l'unité sur le contrôle"
+              />
+              {showCheckbox && <MissionActionIncidentDonwload />}
+            </Stack.Item>
+          )}
           <Stack.Item style={{ width: '100%' }}>
-            <FormikTextAreaInput
-              isLight={false}
-              name="observationsByUnit"
-              data-testid="observations-by-unit"
-              label="Observations de l'inspecteur"
-            />
-            {showCheckbox && <MissionActionIncidentDonwload />}
-            <FormikCheckbox
-              isLight
-              style={{ marginTop: 8 }}
-              name="sati.principalInspector.party.signature"
-              label="Rapport de contrôle signé par l’inspecteur"
-            />
-          </Stack.Item>
-          <Stack.Item style={{ width: '100%' }}>{showCheckbox && <MissionActionDivingOperation />}</Stack.Item>
-          <Stack.Item style={{ width: '100%' }}>
-            <Divider style={{ backgroundColor: THEME.color.lightGray, margin: 0 }} />
-          </Stack.Item>
-          <Stack.Item style={{ width: '100%' }}>
-            <FormikTextAreaInput
-              isLight={false}
-              name="sati.vessel.master.comments"
-              data-testid="observations-master"
-              label="Observations de l’opérateur (personne contrôlée)"
-            />
-            <FormikCheckbox
-              isLight
-              style={{ marginTop: 8 }}
-              name="sati.vessel.master.signature"
-              label="Rapport de contrôle signé par l’opérateur (personne contrôlée)"
-            />
+            {isSatiEnabled && (
+              <Stack direction="column" spacing="0.5rem" alignItems="flex-start" style={{ width: '100%' }}>
+                <Stack.Item style={{ width: '100%' }}>
+                  <FormikTextAreaInput
+                    isLight={false}
+                    name="observationsByUnit"
+                    data-testid="observations-by-unit"
+                    label="Observations de l'inspecteur"
+                  />
+                  {showCheckbox && <MissionActionIncidentDonwload />}
+                  <FormikCheckbox
+                    isLight
+                    style={{ marginTop: 8 }}
+                    name="sati.principalInspector.party.signature"
+                    label="Rapport de contrôle signé par l’inspecteur"
+                  />
+                </Stack.Item>
+                <Stack.Item style={{ width: '100%' }}>{showCheckbox && <MissionActionDivingOperation />}</Stack.Item>
+                <Stack.Item style={{ width: '100%' }}>
+                  <Divider style={{ backgroundColor: THEME.color.lightGray, margin: 0 }} />
+                </Stack.Item>
+                <Stack.Item style={{ width: '100%' }}>
+                  <FormikTextAreaInput
+                    isLight={false}
+                    name="sati.vessel.master.comments"
+                    data-testid="observations-master"
+                    label="Observations de l’opérateur (personne contrôlée)"
+                  />
+                  <FormikCheckbox
+                    isLight
+                    style={{ marginTop: 8 }}
+                    name="sati.vessel.master.signature"
+                    label="Rapport de contrôle signé par l’opérateur (personne contrôlée)"
+                  />
+                </Stack.Item>
+              </Stack>
+            )}
           </Stack.Item>
         </Stack>
       </Stack.Item>
